9f2207468f5f7770957101b69134cdef4d67009b
Brooks Johnson <2008881+bkjohnson@users.noreply.github.com>
2020-07-30 09:21:58 -0700

    Remove unused key-value pairs from bucket consts (#13588)
    These can cause confusion
    
    Signed-off-by: Brooks Johnson <brooks.johnson@pm.me>

--- src/site/constants/buckets.js
+++ src/site/constants/buckets.js
@@ -1,24 +1,14 @@
 const {
   LOCALHOST,
-  DEVELOPMENT,
-  PRODUCTION,
-  STAGING,
   VAGOVDEV,
   VAGOVSTAGING,
   VAGOVPROD,
 } = require('./environments');
 
-const hostnames = require('./hostnames');
-
-const bucket = 'https://s3-us-gov-west-1.amazonaws.com';
-
 const prodBucket = 'https://prod-va-gov-assets.s3-us-gov-west-1.amazonaws.com';
 const devBucket = 'https://dev-va-gov-assets.s3-us-gov-west-1.amazonaws.com';
 
 module.exports = {
-  [DEVELOPMENT]: `${bucket}/${hostnames[DEVELOPMENT]}`,
-  [PRODUCTION]: `${bucket}/${hostnames[PRODUCTION]}`,
-  [STAGING]: `${bucket}/${hostnames[STAGING]}`,
   [VAGOVDEV]: devBucket,
   [VAGOVSTAGING]:
     'https://staging-va-gov-assets.s3-us-gov-west-1.amazonaws.com',
