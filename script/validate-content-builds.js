79be9f1c737342767937227cf9cd819463c04c1d
Bill Fienberg <billfienberg@gmail.com>
2020-11-23 16:02:10 -0600

    Replace references to content-build with references to vets-website

--- script/validate-content-builds.js
+++ script/validate-content-builds.js
@@ -3,11 +3,11 @@ const { runCommandSync } = require('./utils');
 // Run the vets-website content build
 runCommandSync('npm run build:content --pull-drupal');
 
-// Copy the drupal cache into the content-build repo
-runCommandSync('cp -r .cache ../content-build/.cache');
+// Copy the drupal cache from content-build into the vets-website repo
+runCommandSync('cp -r .cache ../vets-website/.cache');
 
 // Run the standalone content build using the same cache
-runCommandSync('cd ../content-build && npm run build:content');
+runCommandSync('cd ../vets-website && npm run build:content');
 
 // Compare the build outputs to see if they match
 const exitCode = runCommandSync('npm run build:compare');
