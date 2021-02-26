ef1decd2912d08d167fa4c7a6df61b26d8f0591d
Tim Wright <timwright12@gmail.com>
2021-02-26 15:53:44 -0500

    save

--- script/validate-content-builds.js
+++ script/validate-content-builds.js
@@ -1,11 +1,13 @@
 const { runCommandSync } = require('./utils');
 
 // run vets-website build
-runCommandSync('cd ../vets-website/ && yarn build --omitdebug --pull-drupal');
+runCommandSync(
+  'cd ../vets-website/ && yarn fetch-drupal-cache && yarn build --omitdebug',
+);
 // move vets-website .cache into content-build
-runCommandSync('cp -r ../vets-website/.cache ./');
+// runCommandSync('cp -r ../vets-website/.cache ./');
 // run content-build build
-runCommandSync('yarn build --omitdebug --port 3001');
+runCommandSync('yarn fetch-drupal-cache && yarn build --omitdebug --port 3001');
 
 // Compare the build outputs to see if they match
 const exitCode = runCommandSync('yarn build:compare');
