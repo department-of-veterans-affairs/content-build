60070534b886716a8f81a835ed5e62b7e345b140
Tim Wright <timwright12@gmail.com>
2021-03-01 09:58:08 -0500

    added 16384 memory

--- jenkins/common.groovy
+++ jenkins/common.groovy
@@ -265,7 +265,7 @@ def buildAll(String ref, dockerContainer, Boolean contentOnlyBuild) {
 
 def prearchive(dockerContainer, envName) {
   dockerContainer.inside(DOCKER_ARGS) {
-    sh "cd /application && node --max-old-space-size=8192 script/prearchive.js --buildtype=${envName}"
+    sh "cd /application && node --max-old-space-size=16384 script/prearchive.js --buildtype=${envName}"
   }
 }
 
