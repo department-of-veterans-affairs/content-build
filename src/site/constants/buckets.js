93446a32b0881f214e60362a6a0a7e901bf3ee5c
Tim Wright <timwright12@gmail.com>
2021-03-10 09:22:27 -0500

    Updated to use apps bucket for dev

--- src/site/constants/buckets.js
+++ src/site/constants/buckets.js
@@ -6,12 +6,14 @@ const {
 } = require('./environments');
 
 const prodBucket = 'https://prod-va-gov-assets.s3-us-gov-west-1.amazonaws.com';
-const devBucket = 'https://dev-va-gov-assets.s3-us-gov-west-1.amazonaws.com';
+const stagingBucket =
+  'https://dev-va-gov-assets.s3-us-gov-west-1.amazonaws.com';
+const devBucket =
+  'http://apps.dev.va.gov.s3-website-us-gov-west-1.amazonaws.com/';
 
 module.exports = {
   [VAGOVDEV]: devBucket,
-  [VAGOVSTAGING]:
-    'https://staging-va-gov-assets.s3-us-gov-west-1.amazonaws.com',
+  [VAGOVSTAGING]: stagingBucket,
   [VAGOVPROD]: prodBucket,
   [LOCALHOST]: devBucket,
 };
