98f5c1c046452918d6d5def920ac0bebea3842bb
Theo Bentum <theo@adhocteam.us>
2021-03-16 13:49:03 -0700

    Update bucket urls for apps and content

--- src/site/constants/buckets-content.js
+++ src/site/constants/buckets-content.js
@@ -8,8 +8,7 @@ const {
 const prodBucket = 'https://prod-va-gov-assets.s3-us-gov-west-1.amazonaws.com';
 const stagingBucket =
   'https://staging-va-gov-assets.s3-us-gov-west-1.amazonaws.com';
-const devBucket =
-  'http://content.dev.va.gov.s3-website-us-gov-west-1.amazonaws.com';
+const devBucket = 'https://s3-us-gov-west-1.amazonaws.com/content.dev.va.gov';
 
 module.exports = {
   [VAGOVDEV]: devBucket,
