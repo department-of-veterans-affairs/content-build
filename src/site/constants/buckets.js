01631eb700c88d29d64e2da363c1305ff26b2b28
Theo Bentum <theo@adhocteam.us>
2021-03-16 10:33:10 -0700

    Update asset bucket for dev

--- src/site/constants/buckets.js
+++ src/site/constants/buckets.js
@@ -8,7 +8,7 @@ const {
 const prodBucket = 'https://prod-va-gov-assets.s3-us-gov-west-1.amazonaws.com';
 const stagingBucket =
   'https://staging-va-gov-assets.s3-us-gov-west-1.amazonaws.com';
-const devBucket = 'https://dev-va-gov-assets.s3-us-gov-west-1.amazonaws.com';
+const devBucket = 'http://apps.dev.va.gov.s3-website-us-gov-west-1.amazonaws.com/';
 
 module.exports = {
   [VAGOVDEV]: devBucket,
