6c3bfcf465d3bc1319e235376d3ab14abc08be69
Tim Wright <timwright12@gmail.com>
2021-03-23 16:22:32 -0400

    added install script to match vets-website

---
+++ script/install-repos.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+if [ ! -d ../vagov-content ]; then
+  git clone --single-branch --depth 1 https://github.com/department-of-veterans-affairs/vagov-content.git ../vagov-content
+else
+  echo "Repo vagov-content already cloned."
+fi
+
+if [ ! -d ../vets-website ]; then
+  git clone --single-branch --depth 1 https://github.com/department-of-veterans-affairs/vets-website.git ../vets-website
+else
+  echo "Repo vets-website already cloned."
+fi
