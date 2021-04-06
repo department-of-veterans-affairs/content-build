1a44a05720fc9d47056d0b2cd8f5f731066eed1e
Tim Wright <timwright12@gmail.com>
2021-04-06 17:02:36 -0400

    updated bucket paths

--- src/site/constants/buckets.js
+++ src/site/constants/buckets.js
@@ -5,7 +5,7 @@ const {
   VAGOVPROD,
 } = require('./environments');
 
-const prodBucket = 'https://prod-va-gov-assets.s3-us-gov-west-1.amazonaws.com';
+const prodBucket = 'https://s3-us-gov-west-1.amazonaws.com/apps.www.va.gov';
 const stagingBucket =
   'https://s3-us-gov-west-1.amazonaws.com/apps.staging.va.gov';
 const devBucket = 'https://s3-us-gov-west-1.amazonaws.com/apps.dev.va.gov';
