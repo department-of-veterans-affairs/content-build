ae004bae13fc1dd51974ed90c81d242a7e810ba4
Theo Bentum <theo@adhocteam.us>
2021-04-19 13:59:43 -0700

    Run Cypress tests in Jenkins

--- jenkins/common.groovy
+++ jenkins/common.groovy
@@ -357,6 +357,10 @@ def integrationTests(dockerContainer, ref) {
             'nightwatch-accessibility': {
               sh "export IMAGE_TAG=${IMAGE_TAG} && docker-compose -p accessibility-${env.EXECUTOR_NUMBER} up -d && docker-compose -p accessibility-${env.EXECUTOR_NUMBER} run --rm --entrypoint=npm -e BABEL_ENV=test -e BUILDTYPE=vagovprod content-build --no-color run nightwatch:docker -- --env=accessibility"
             },
+
+            cypress: {
+              sh "export IMAGE_TAG=${commonStages.IMAGE_TAG} && docker-compose -p cypress up -d && docker-compose -p cypress run --rm --entrypoint=npm -e CI=true -e NO_COLOR=1 vets-website --no-color run cy:test:docker"
+            }
           )
         } else {
           parallel (
@@ -364,6 +368,10 @@ def integrationTests(dockerContainer, ref) {
 
             'nightwatch-e2e': {
               sh "export IMAGE_TAG=${IMAGE_TAG} && docker-compose -p nightwatch-${env.EXECUTOR_NUMBER} up -d && docker-compose -p nightwatch-${env.EXECUTOR_NUMBER} run --rm --entrypoint=npm -e BABEL_ENV=test -e BUILDTYPE=vagovprod content-build --no-color run nightwatch:docker"
+            }
+
+            cypress: {
+              sh "export IMAGE_TAG=${commonStages.IMAGE_TAG} && docker-compose -p cypress up -d && docker-compose -p cypress run --rm --entrypoint=npm -e CI=true -e NO_COLOR=1 vets-website --no-color run cy:test:docker"
             }
           )
         }
