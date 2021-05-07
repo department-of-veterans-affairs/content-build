61ccb71cb302913e77a05b6f8ec8b00679e262bd
Chris Alexander <chris.alexander@adhocteam.us>
2021-05-07 11:11:49 -0600

    Add --trace_gc to show garbage collection stats

--- src/site/stages/build/plugins/modify-dom/index.js
+++ src/site/stages/build/plugins/modify-dom/index.js
@@ -42,6 +42,8 @@ const modifyDom = BUILD_OPTIONS => files => {
   // at once would cause a massive amount of memory to be consumed.
   for (const [fileName, file] of Object.entries(files)) {
     if (path.extname(fileName) === '.html') {
+      // eslint-disable-next-line no-console
+      console.log(`+ ${fileName} `);
       file.dom = cheerio.load(file.contents);
       for (const modifier of domModifiers) {
         modifier.modifyFile(fileName, file, files, BUILD_OPTIONS);
