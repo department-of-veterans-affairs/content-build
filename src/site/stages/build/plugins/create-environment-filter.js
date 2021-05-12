e1a5ea432b7252bbf00388726c48b81c77c693a5
Nick Sullivan <nick.sullivan@adhocteam.us>
2021-05-12 15:23:07 -0400

    Update create-environment-filter.js

--- src/site/stages/build/plugins/create-environment-filter.js
+++ src/site/stages/build/plugins/create-environment-filter.js
@@ -8,14 +8,6 @@ function createEnvironmentFilter(options) {
     for (const fileName of Object.keys(files)) {
       const file = files[fileName];
 
-      // Do not include CLPs on production (except for the preview server).
-      if (
-        !options.isPreviewServer &&
-        environmentName === ENVIRONMENTS.VAGOVPROD
-      ) {
-        delete files[fileName];
-      }
-
       if (
         environmentName !== ENVIRONMENTS.LOCALHOST &&
         file.status === 'draft'
