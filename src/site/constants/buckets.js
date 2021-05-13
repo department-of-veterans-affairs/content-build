097e310bba6afeb5e1cacbe184006cad6b48df23
Theo Bentum <theo@adhocteam.us>
2021-05-13 12:32:23 -0700

    Revert bucket url to old url

--- src/site/constants/buckets.js
+++ src/site/constants/buckets.js
@@ -7,8 +7,8 @@ const {
 
 const prodBucket = 'https://prod-va-gov-assets.s3-us-gov-west-1.amazonaws.com';
 const stagingBucket =
-  'https://s3-us-gov-west-1.amazonaws.com/apps.staging.va.gov';
-const devBucket = 'https://s3-us-gov-west-1.amazonaws.com/apps.dev.va.gov';
+  'https://staging-va-gov-assets.s3-us-gov-west-1.amazonaws.com';
+const devBucket = 'https://dev-va-gov-assets.s3-us-gov-west-1.amazonaws.com';
 
 module.exports = {
   [VAGOVDEV]: devBucket,
