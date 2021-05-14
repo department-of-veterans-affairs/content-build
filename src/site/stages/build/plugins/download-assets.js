a15de10c24ecfe8391b89d15b62aef1503422d22
Theo Bentum <theo@adhocteam.us>
2021-05-14 09:56:57 -0700

    Add conditional for download URL

--- src/site/stages/build/plugins/download-assets.js
+++ src/site/stages/build/plugins/download-assets.js
@@ -54,7 +54,9 @@ async function downloadFromLiveBucket(files, buildOptions) {
 
   const downloads = entryNames.map(async entryName => {
     let bundleFileName = fileManifest[entryName];
-    const bundleUrl = `${bucket}${bundleFileName}`;
+    const bundleUrl = bundleFileName.includes(`${bucket}`)
+      ? `${bundleFileName}`
+      : `${bucket}${bundleFileName}`;
     const bundleResponse = await fetch(bundleUrl);
 
     if (bundleFileName.includes('generated/../')) {
