9ff0deac715b118a5e4d02b891bc20d790a2a4fe
Chris Alexander <chris.alexander@adhocteam.us>
2021-05-18 16:59:16 -0600

    Add sleep to rewrite domains step as well

--- src/site/stages/build/plugins/rewrite-va-domains.js
+++ src/site/stages/build/plugins/rewrite-va-domains.js
@@ -1,32 +1,33 @@
 /* eslint-disable no-param-reassign, no-continue */
 
 function createRedirects(options) {
-  return (files, metalsmith, done) => {
+  return async (files, metalsmith, done) => {
     if (options.domainReplacements) {
+      const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
       let itemsSinceCall = 0;
 
-      Object.keys(files)
-        .filter(fileName => fileName.endsWith('html'))
-        .forEach(fileName => {
-          const file = files[fileName];
-          let contents = file.contents.toString();
-          delete file.contents;
-          options.domainReplacements.forEach(domain => {
-            const regex = new RegExp(domain.from, 'g');
-            contents = contents.replace(regex, domain.to);
-          });
-
-          file.contents = Buffer.from(contents);
-
-          itemsSinceCall++;
-          if (itemsSinceCall > 1000) {
-            itemsSinceCall = 0;
-            global.gc();
-            /* eslint-disable no-console */
-            console.log('global.gc() in createRedirects() at 1000 items');
-          }
+      const htmlFiles = Object.keys(files).filter(filename => {
+        return filename.endsWith('html');
+      });
+      for (const fileName of htmlFiles) {
+        const file = files[fileName];
+        let contents = file.contents.toString();
+        delete file.contents;
+        options.domainReplacements.forEach(domain => {
+          const regex = new RegExp(domain.from, 'g');
+          contents = contents.replace(regex, domain.to);
         });
 
+        file.contents = Buffer.from(contents);
+
+        itemsSinceCall++;
+        if (itemsSinceCall > 100) {
+          itemsSinceCall = 0;
+          /* eslint-disable no-await-in-loop, no-console */
+          await sleep(1);
+          console.log('sleep for 1ms at 100 items');
+        }
+      }
       // const replacementsWithRegex = options.domainReplacements.map(domain => ({
       //   ...domain,
       //   regex: new RegExp(domain.from, 'g'),
