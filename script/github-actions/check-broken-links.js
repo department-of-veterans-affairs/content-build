03d2ab640f58480195462c3bf7e853ff566d3a2f
Oseas Moran <oseas.moran@adhocteam.us>
2021-05-18 11:13:23 -0700

    testing slack message

--- script/github-actions/check-broken-links.js
+++ script/github-actions/check-broken-links.js
@@ -10,6 +10,34 @@ const BRANCH_NAME = process.env.GITHUB_REF;
 const IS_PROD_BRANCH = BRANCH_NAME.replace('refs/heads/', '') === 'master';
 const maxBrokenLinks = 10;
 
+const testMessage = `{
+  "attachments": [
+    {
+      "color": "#000",
+      "blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "HEADING BROKEN LINKS"
+          }
+        },
+        {
+          "type": "divider"
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "BROKENLINK_SUMMARY_HERE"
+          }
+        }
+      ]
+    }
+  ]
+}`;
+console.log(`::set-output name=SLACK_MESSAGE_${envName}::${testMessage}`);
+
 // broken links detected
 if (fs.existsSync(reportPath)) {
   const brokenLinksReport = fs.readFileSync(reportPath, 'utf8');
@@ -17,17 +45,45 @@ if (fs.existsSync(reportPath)) {
   const shouldFail =
     brokenLinks.isHomepageBroken ||
     brokenLinks.brokenLinksCount > maxBrokenLinks;
-  const color = shouldFail ? 'danger' : 'warning';
+  const color = shouldFail ? '#D33834' : '#FFCC00'; // danger or warning, needs to be in hex
 
   const heading = `@cmshelpdesk ${brokenLinks.brokenLinksCount} broken links found in the ${envName} build on ${BRANCH_NAME} \n\n${SERVER_URL}\n\n`;
-  const message = `${heading}\n${brokenLinks.summary}`;
+  // const message = `${heading}\n${brokenLinks.summary}`;
+
+  const message = `{
+    "attachments": [
+      {
+        "color": "${color}",
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "${heading}"
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "${brokenLinks.summary}"
+            }
+          }
+        ]
+      }
+    ]
+  }`;
 
   console.log(
     `${brokenLinks.brokenLinksCount} broken links found. \n ${brokenLinks.summary}`,
   );
 
-  console.log(`::set-output name=SLACK_MESSAGE::${message}`);
-  console.log(`::set-output name=SLACK_COLOR::${color}`);
+  console.log(`::set-output name=SLACK_MESSAGE_${envName}::${message}`);
+  console.log(`::set-output name=${envName}_SHOULD_FAIL::${shouldFail}`);
+  // console.log(`::set-output name=SLACK_COLOR_${envName}::${color}`);
 
   if (!IS_PROD_BRANCH && !contentOnlyBuild) {
     // Ignore the results of the broken link checker unless
