4684b566109abc34e9d5d5978f27468c5e1852c1
Oseas Moran <oseas.moran@adhocteam.us>
2021-05-21 11:32:25 -0700

    reintroduce color to slack format

--- script/github-actions/check-broken-links.js
+++ script/github-actions/check-broken-links.js
@@ -19,12 +19,11 @@ if (fs.existsSync(reportPath)) {
   const shouldFail =
     brokenLinks.isHomepageBroken ||
     brokenLinks.brokenLinksCount > maxBrokenLinks;
-  // const color = shouldFail ? '#D33834' : '#FFCC00'; // danger or warning, needs to be in hex
+  const color = shouldFail ? '#D33834' : '#FFCC00'; // danger or warning, needs to be in hex
   // const heading = `@cmshelpdesk ${brokenLinks.brokenLinksCount} broken links found in the ${envName} build on ${BRANCH_NAME} \n\n${SERVER_URL}\n\n`;
   // const slackBlocks = `[{"type": "section", "text": {"type": "plain_text", "text": "${heading}"}}]`;
   const slackBlocks = `[{"type": "section","text": {"type": "plain_text","text": "@CMS Team, # broken link found -- heading "}}]`;
-  const slackAttachments =
-    '[{"color": "#D33834", "text": "broken_link_summary information"}]';
+  const slackAttachments = `[{"color": "${color}", "text": "broken_link_summary information"}]`;
   // const slackAttachments = `[{"color": "${color}", "text": "${brokenLinks.summary}"}]`;
 
   console.log(
