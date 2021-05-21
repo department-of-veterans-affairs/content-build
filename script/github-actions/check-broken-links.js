f1f2dc8b61ddf2e06a6943f53a55b16e36f439e6
Oseas Moran <oseas.moran@adhocteam.us>
2021-05-21 11:13:26 -0700

    remove all dynamic code

--- script/github-actions/check-broken-links.js
+++ script/github-actions/check-broken-links.js
@@ -19,7 +19,7 @@ if (fs.existsSync(reportPath)) {
   const shouldFail =
     brokenLinks.isHomepageBroken ||
     brokenLinks.brokenLinksCount > maxBrokenLinks;
-  const color = shouldFail ? '#D33834' : '#FFCC00'; // danger or warning, needs to be in hex
+  // const color = shouldFail ? '#D33834' : '#FFCC00'; // danger or warning, needs to be in hex
   // const heading = `@cmshelpdesk ${brokenLinks.brokenLinksCount} broken links found in the ${envName} build on ${BRANCH_NAME} \n\n${SERVER_URL}\n\n`;
   // const slackBlocks = `[{"type": "section", "text": {"type": "plain_text", "text": "${heading}"}}]`;
   const slackBlocks = `[
@@ -31,7 +31,9 @@ if (fs.existsSync(reportPath)) {
       },
     },
   ]`;
-  const slackAttachments = `[{"color": "${color}", "text": "${brokenLinks.summary}"}]`;
+  const slackAttachments =
+    '[{"color": "#D33834", "text": "broken_link_summary information"}]';
+  // const slackAttachments = `[{"color": "${color}", "text": "${brokenLinks.summary}"}]`;
 
   console.log(
     `${brokenLinks.brokenLinksCount} broken links found. \n ${brokenLinks.summary}`,
