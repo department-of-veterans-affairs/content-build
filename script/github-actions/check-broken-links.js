f8d65143319acc2f72141a8e996ec3406eeebbb3
Oseas Moran <oseas.moran@adhocteam.us>
2021-05-24 07:34:15 -0700

    test double backslash on string format

--- script/github-actions/check-broken-links.js
+++ script/github-actions/check-broken-links.js
@@ -23,7 +23,7 @@ if (fs.existsSync(reportPath)) {
   const heading = `@cmshelpdesk ${brokenLinks.brokenLinksCount} broken links found in ${envName} <${SERVER_URL}>`;
   const slackBlocks = `[{"type": "section","text": {"type": "mrkdwn","text": "${heading}"}}]`;
   // const slackAttachments = `[{"mrkdwn_in": ["text"], "color": "${color}", "text": ${brokenLinks.summary} }]`;
-  const slackAttachments = `[{"mrkdwn_in": ["text"], "color": "${color}", "text": "*\`palo-alto-health-care/chaplain-services\`* \n \`\`\` <a> \`\`\` : "}]`;
+  const slackAttachments = `[{"mrkdwn_in": ["text"], "color": "${color}", "text": "*\`palo-alto-health-care/chaplain-services\`* \\n \`\`\` <a> \`\`\` : "}]`;
   console.log(
     `${brokenLinks.brokenLinksCount} broken links found. \n ${brokenLinks.summary}`,
   );
