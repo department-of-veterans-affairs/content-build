458a11c624d4b383edc631ea0abac9da410eb4b5
Kelson Adams <kadams@governmentcio.com>
2021-05-26 14:39:35 -0600

    Update pages to be omitted from production

--- src/site/stages/build/plugins/create-environment-filter.js
+++ src/site/stages/build/plugins/create-environment-filter.js
@@ -8,11 +8,13 @@ function createEnvironmentFilter(options) {
     for (const fileName of Object.keys(files)) {
       const file = files[fileName];
 
-      // Do not include random-lang-support-link on production (except for the preview server).
+      // Do not include the following pages on production (except for the preview server):
+      // va.gov/asistencia-y-recursos-en-espanol
+      // va.gov/tagalog-wika-mapagkukunan-at-tulong
       if (
         !options.isPreviewServer &&
         environmentName === ENVIRONMENTS.VAGOVPROD &&
-        file.entityBundle === 'random-lang-support-link'
+        (file.entityId === '20078' || file.entityId === '20092')
       ) {
         delete files[fileName];
       }
