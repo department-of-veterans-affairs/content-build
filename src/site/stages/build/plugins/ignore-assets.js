0a512c08bc1950abbdb411e6f5938ac96bb6c891
Theo Bentum <theo@adhocteam.us>
2021-06-08 19:39:53 +0000

    Remove downloaded file contents from file object (#216)
    * Add extra memory stats
    
    * Remove file contents from drupal asset files
    
    * Remove file contents from asset files
    
    * Test with 48k nodes
    
    * Remove outputFileSync when downloading assets
    
    * Changes for testing 48k nodes
    
    * Add comment for storing file contents
    
    * Undo changes for testing on 48k nodes
    
    * Undo change to drupal/page.js
    
    * Store loaded Drupal cache on disk
    
    * Store file manifest on disk
    
    * Store Drupal assets directly in build directory
    
    * Store app assets directly on disk
    
    * Add plugin for removing assets from build
    
    * Keep headerFooter.json in build
    
    * Remove unused download functions

---
+++ src/site/stages/build/plugins/ignore-assets.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-param-reassign */
+
+function ignoreAssets() {
+  return (files, metalsmith, done) => {
+    Object.entries(files)
+      .filter(
+        ([fileName, file]) =>
+          file.isDrupalAsset ||
+          (fileName.includes('generated/') &&
+            fileName !== 'generated/headerFooter.json'),
+      )
+      .forEach(([fileName]) => {
+        delete files[fileName];
+      });
+    done();
+  };
+}
+
+module.exports = ignoreAssets;
