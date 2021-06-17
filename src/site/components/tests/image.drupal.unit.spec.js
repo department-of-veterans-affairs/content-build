152bad6cafe1d6bab78de9e272f8da0199cb2717
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2021-06-17 18:02:59 -0400

    Do not render images if they don't contain the required data (url and alt).
    Resolves https://github.com/department-of-veterans-affairs/va.gov-team/issues/25599

---
+++ src/site/components/tests/image.drupal.unit.spec.js
@@ -0,0 +1,52 @@
+import { expect } from 'chai';
+import { renderHTML } from '~/site/tests/support';
+
+const layoutPath = 'src/site/components/image.drupal.liquid';
+
+describe('Facility Listing', () => {
+  it('renders img tag if url and alt are present', async () => {
+    const data = {
+      url: 'testUrl', // TODO: validate the url format?
+      alt: 'testAlt',
+      class: 'testClass',
+    };
+
+    const container = await renderHTML(layoutPath, data);
+
+    expect(container.querySelector('img').outerHTML).to.equal(
+      '<img class="testClass" src="testUrl" alt="testAlt">',
+    );
+  });
+
+  it('does NOT render img tag if url is not present', async () => {
+    const data = {
+      alt: 'testAlt',
+      class: 'testClass',
+    };
+
+    const container = await renderHTML(layoutPath, data);
+
+    expect(container.querySelector('img')).not.to.exist;
+  });
+
+  it('does NOT render img tag if alt is not present', async () => {
+    const data = {
+      url: 'testUrl',
+      class: 'testClass',
+    };
+
+    const container = await renderHTML(layoutPath, data);
+
+    expect(container.querySelector('img')).not.to.exist;
+  });
+
+  it('does NOT render img tag if neither url nor alt is present', async () => {
+    const data = {
+      class: 'testClass',
+    };
+
+    const container = await renderHTML(layoutPath, data);
+
+    expect(container.querySelector('img')).not.to.exist;
+  });
+});
