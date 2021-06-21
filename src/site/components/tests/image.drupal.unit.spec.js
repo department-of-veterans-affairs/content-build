2eb958b2ed5e566387fdbd8bde92851a4830accd
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2021-06-21 19:07:12 -0400

    Use image object for image template

--- src/site/components/tests/image.drupal.unit.spec.js
+++ src/site/components/tests/image.drupal.unit.spec.js
@@ -6,12 +6,16 @@ const layoutPath = 'src/site/components/image.drupal.liquid';
 describe('Facility Listing', () => {
   it('renders img tag if url and alt are present', async () => {
     const data = {
-      url: 'testUrl', // TODO: validate the url format?
-      alt: 'testAlt',
+      image: {
+        derivative: {
+          url: 'testUrl', // TODO: validate the url format?
+          width: 'testWidth',
+          height: 'testHeight',
+        },
+        alt: 'testAlt',
+        title: 'testTitle',
+      },
       class: 'testClass',
-      title: 'testTitle',
-      width: 'testWidth',
-      height: 'testHeight',
     };
 
     const container = await renderHTML(layoutPath, data);
@@ -23,8 +27,15 @@ describe('Facility Listing', () => {
 
   it('does NOT render img tag if url is not present', async () => {
     const data = {
-      alt: 'testAlt',
-      class: 'testClass',
+      image: {
+        derivative: {
+          width: 'testWidth',
+          height: 'testHeight',
+        },
+        alt: 'testAlt',
+        class: 'testClass',
+        title: 'testTitle',
+      },
     };
 
     const container = await renderHTML(layoutPath, data);
@@ -34,8 +45,15 @@ describe('Facility Listing', () => {
 
   it('does NOT render img tag if alt is not present', async () => {
     const data = {
-      url: 'testUrl',
-      class: 'testClass',
+      image: {
+        derivative: {
+          url: 'testUrl',
+          width: 'testWidth',
+          height: 'testHeight',
+        },
+        class: 'testClass',
+        title: 'testTitle',
+      },
     };
 
     const container = await renderHTML(layoutPath, data);
