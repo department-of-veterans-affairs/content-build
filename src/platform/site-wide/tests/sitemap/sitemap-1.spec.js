cf635c07e26fb5daa9083865717b13da2334e739
Tim Wright <timwright12@gmail.com>
2021-06-24 12:21:11 -0400

    Added comment to trigger new test run

--- src/platform/site-wide/tests/sitemap/sitemap-1.spec.js
+++ src/platform/site-wide/tests/sitemap/sitemap-1.spec.js
@@ -8,6 +8,7 @@ const Timeouts = require('../../../testing/e2e/timeouts.js');
 
 module.exports = {
   'sitemap 1/4': client => {
+    // Setting a large timeout so reduced memory doesn't cause failures
     client.timeoutsAsyncScript(Timeouts.verySlow);
     SitemapHelpers.sitemapURLs().then(function runFirstAxeCheck({
       urls,
--- src/platform/site-wide/tests/sitemap/sitemap-3.spec.js
+++ src/platform/site-wide/tests/sitemap/sitemap-3.spec.js
@@ -3,6 +3,7 @@ const Timeouts = require('../../../testing/e2e/timeouts.js');
 
 module.exports = {
   'sitemap 3/4': client => {
+    // Setting a large timeout so reduced memory doesn't cause failures
     client.timeoutsAsyncScript(Timeouts.verySlow);
     SitemapHelpers.sitemapURLs().then(function runThirdAxeCheck({
       urls,
