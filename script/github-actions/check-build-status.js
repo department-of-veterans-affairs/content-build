934f76823314ab3c0c472caa5d18b2822aea0292
Oseas Moran <oseas.moran@adhocteam.us>
2021-06-28 14:51:54 -0700

    refactor check-build-status

--- script/github-actions/check-build-status.js
+++ script/github-actions/check-build-status.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-console */
+/* eslint-disable camelcase */
 const fetch = require('node-fetch');
 
 const args = process.argv.slice(2);
 const headSHA = args[0];
-const timeout = args[1] ? args[1] : 120;
+const timeout = 5; // minutes
 const checkRunURL = `https://api.github.com/repos/department-of-veterans-affairs/content-build/commits/${headSHA}/check-runs`;
 let commitNull = false;
 
@@ -11,24 +12,21 @@ function getLatestCheckRun(URL) {
   const headers = { Accept: 'application/vnd.github.v3+json' };
   return fetch(URL, headers)
     .then(response => response.json())
-    .then(githubObject => {
-      for (let i = 0; i < Object.keys(githubObject.check_runs).length; i++) {
-        if (
-          githubObject.check_runs[i].conclusion === 'failure' ||
-          commitNull === true
-        ) {
+    .then(({ check_runs }) => {
+      const validCheckRuns = check_runs.filter(
+        ({ name }) => name !== 'Accessibility Scan',
+      );
+      for (let i = 0; i < validCheckRuns.length; i++) {
+        // html_url specific github key. ignoring camelcase lint
+        const { conclusion, html_url, status } = validCheckRuns[i];
+        if (conclusion === 'failure' || commitNull === true) {
           const headerMessage = commitNull
             ? 'Build aborted due to check runs still in progress on'
             : 'Build aborted due to failed runs detected on';
-          throw new Error(
-            `${headerMessage} ${headSHA}.\n\n ${githubObject.check_runs[i].html_URL}`,
-          );
-        } else if (
-          githubObject.check_runs[i].status === 'in_progress' ||
-          githubObject.check_runs[i].status === null
-        ) {
+          throw new Error(`${headerMessage} ${headSHA}.\n\n ${html_url}`);
+        } else if (status === 'in_progress' || status === null) {
           commitNull = true;
-          return;
+          return; // break for loop to wait for check_run to finish in progress
         }
       }
     })
@@ -38,13 +36,13 @@ function getLatestCheckRun(URL) {
     });
 }
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms * 1000));
+function sleep(minutes) {
+  return new Promise(resolve => setTimeout(resolve, minutes * 60 * 1000));
 }
 
 async function main() {
   await getLatestCheckRun(checkRunURL);
-  if (!commitNull) {
+  if (commitNull) {
     console.log(`Check runs still pending. Sleeping for ${timeout}s`);
     await sleep(timeout);
     await getLatestCheckRun(checkRunURL);
