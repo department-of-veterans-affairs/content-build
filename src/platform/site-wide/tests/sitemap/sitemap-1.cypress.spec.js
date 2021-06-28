b27028095309f95ce487aa62fdf67f5ddada97ae
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-06-28 09:58:05 -0500

    WIP

--- src/platform/site-wide/tests/sitemap/sitemap-1.cypress.spec.js
+++ src/platform/site-wide/tests/sitemap/sitemap-1.cypress.spec.js
@@ -1,35 +1,33 @@
 const { normal } = require('../../../../platform/testing/e2e/timeouts');
 const xml = require('fast-xml-parser');
-const fetch = require("sync-fetch");
+const fetch = require('sync-fetch');
 
 const options = {
-    attributeNamePrefix : "@_",
-    attrNodeName: "attr", //default is 'false'
-    textNodeName : "#text",
-    ignoreAttributes : true,
-    ignoreNameSpace : true,
-    allowBooleanAttributes : false,
-    parseNodeValue : true,
-    parseAttributeValue : false,
-    trimValues: true,
-    cdataTagName: "__cdata", //default is 'false'
-    cdataPositionChar: "\\c",
-    parseTrueNumberOnly: false,
-    // arrayMode: false, //"strict"
-    // attrValueProcessor: (val, attrName) => he.decode(val, {isAttributeValue: true}),//default is a=>a
-    // tagValueProcessor : (val, tagName) => he.decode(val), //default is a=>a
-    stopNodes: ["parse-me-as-string"]
+  attributeNamePrefix: '@_',
+  attrNodeName: 'attr',
+  textNodeName: '#text',
+  ignoreAttributes: true,
+  ignoreNameSpace: true,
+  allowBooleanAttributes: false,
+  parseNodeValue: true,
+  parseAttributeValue: false,
+  trimValues: true,
+  cdataTagName: '__cdata',
+  cdataPositionChar: '\\c',
+  parseTrueNumberOnly: false,
+  stopNodes: ['parse-me-as-string'],
 };
 
 const data = fetch(`http://localhost:3002/sitemap.xml`).text();
-const lol = xml.parse(data, options).urlset.url;
+const urls = xml.parse(data, options).urlset.url;
 
 describe('Accessibility tests', () => {
-//   for (const abc of lol) {
-    it(`${lol[0].loc}`, () => {
-      cy.visit(lol[0].loc).injectAxe();
+  for (const url of urls) {
+    // eslint-disable-next-line no-loop-func
+    it(`${url.loc}`, () => {
+      cy.visit(url.loc).injectAxe();
       cy.get('body').should('be.visible', { timeout: normal });
       cy.axeCheck();
     });
-//   }
+  }
 });
