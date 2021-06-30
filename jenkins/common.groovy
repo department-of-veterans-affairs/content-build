e4f13828b8aa7582c02288014cb66852695999ed
Tim Wright <timwright12@gmail.com>
2021-06-30 14:45:24 -0400

    working commit

--- jenkins/common.groovy
+++ jenkins/common.groovy
@@ -216,7 +216,7 @@ def checkForBrokenLinks(String buildLogPath, String envName, Boolean contentOnly
 
 
 def getQueryStartTime(String buildLogPath, String envName) {
-  def queryStartTime = sh(returnStdout: true, script: "sed -nr 's/Get Drupal content (.+)\\..+/\\1/p' ${buildLogPath} | sort | uniq")
+  // def queryStartTime = sh(returnStdout: true, script: "sed -nr 's/Get Drupal content (.+)\\..+/\\1/p' ${buildLogPath} | sort | uniq")
   // if (queryStartTime) {
   //    echo "${queryStartTime}!"
   // }
