35063e16a9d219fdc83e5db8e2fdfaf2d6611331
Oseas Moran <oseas.moran@adhocteam.us>
2021-06-30 12:59:03 -0700

    check-build until error thrown or test succeeds

--- script/github-actions/check-build-status.js
+++ script/github-actions/check-build-status.js
@@ -3,15 +3,26 @@
 const fetch = require('node-fetch');
 
 const args = process.argv.slice(2);
-const headSHA = args[0];
-const timeout = 5; // minutes
-const checkRunURL = `https://api.github.com/repos/department-of-veterans-affairs/content-build/commits/${headSHA}/check-runs`;
-let commitNull = false;
+const repo = args[0];
+const headSHA = args[1];
+const timeout = 2; // minutes
+const checkRunURL = `https://api.github.com/repos/${repo}/commits/${headSHA}/check-runs`;
 
+/**
+ * fetch request to github action URL provided
+ * @param {string} url
+ */
 function getLatestCheckRun(url) {
   const headers = { Accept: 'application/vnd.github.v3+json' };
   return fetch(url, headers)
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        return Promise.reject(
+          Error(`Response not okay with ${url}. Aborting.`),
+        );
+      }
+      return response.json();
+    })
     .then(({ check_runs }) => {
       const validCheckRuns = check_runs.filter(
         ({ name }) => name !== 'Accessibility Scan',
@@ -19,20 +30,18 @@ function getLatestCheckRun(url) {
       for (let i = 0; i < validCheckRuns.length; i++) {
         // html_url specific github key. ignoring camelcase lint
         const { conclusion, html_url, status } = validCheckRuns[i];
-        if (conclusion === 'failure' || commitNull === true) {
-          const headerMessage = commitNull
-            ? 'Build aborted due to check runs still in progress on'
-            : 'Build aborted due to failed runs detected on';
-          throw new Error(`${headerMessage} ${headSHA}.\n\n ${html_url}`);
+        if (conclusion === 'failure') {
+          return Promise.reject(
+            Error(
+              `Build aborted due to failed runs detected on ${headSHA}.\n\n ${html_url}`,
+            ),
+          );
         } else if (status === 'in_progress' || status === null) {
-          commitNull = true;
-          return; // break for loop to wait for check_run to finish in progress
+          return Promise.reject({});
         }
       }
-    })
-    .catch(error => {
-      console.log(error);
-      process.exit(1);
+      console.log(`All checks succeeded for ${headSHA}`);
+      return Promise.resolve();
     });
 }
 
@@ -40,14 +49,21 @@ function sleep(minutes) {
   return new Promise(resolve => setTimeout(resolve, minutes * 60 * 1000));
 }
 
+/**
+ * Checks Github Actions url. Loops recursively until error is thrown.
+ */
 async function main() {
-  await getLatestCheckRun(checkRunURL);
-  if (commitNull) {
-    console.log(`Check runs still pending. Sleeping for ${timeout}s`);
+  try {
+    return await getLatestCheckRun(checkRunURL);
+  } catch (e) {
+    if (e.name !== undefined && e.name === 'Error') {
+      console.log(e);
+      process.exit(1);
+    }
+    console.log(`Check runs still pending. Sleeping for ${timeout} minutes`);
     await sleep(timeout);
-    await getLatestCheckRun(checkRunURL);
+    return main();
   }
-  console.log(`All checks succeeded for ${headSHA}`);
 }
 
 main();
