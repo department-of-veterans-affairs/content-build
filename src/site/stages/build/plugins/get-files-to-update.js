ea374065504183a6f46d6a54647c30a66da9d7dc
Theo Bentum <theo@adhocteam.us>
2021-07-01 09:34:01 -0700

    Add plugin for getting files related to template changes

---
+++ src/site/stages/build/plugins/get-files-to-update.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-param-reassign, no-console */
+const fs = require('fs');
+const path = require('path');
+
+function getFilesToUpdate() {
+  return (files, metalsmith, done) => {
+    const updatedLayouts = [];
+
+    if (path.basename(path.dirname(global.updatedFilePath)) === 'layouts') {
+      updatedLayouts.push(path.basename(global.updatedFilePath));
+    } else {
+      // Array of all layout paths
+      const layoutPaths = fs
+        .readdirSync(path.join(__dirname, '../../../layouts'), {
+          withFileTypes: true,
+        })
+        .filter(layoutPath => layoutPath.isFile())
+        .map(layoutPath => layoutPath.name);
+
+      // Relative path of updated file
+      const relativePath = path.relative(
+        '../content-build',
+        global.updatedFilePath,
+      );
+
+      layoutPaths.forEach(layoutPath => {
+        const layout = fs.readFileSync(
+          path.join(__dirname, '../../../layouts', layoutPath),
+          'utf8',
+        );
+
+        // If the updated file is referenced in the layout,
+        // add layout to the array of layouts to update
+        if (layout.includes(relativePath)) {
+          updatedLayouts.push(layoutPath);
+        }
+      });
+
+      console.log('Layouts updated:\n', updatedLayouts.join('\n'), '\n');
+    }
+
+    Object.keys(global.metalsmithFiles)
+      .filter(fileName =>
+        updatedLayouts.includes(global.metalsmithFiles[fileName].layout),
+      )
+      .forEach(key => {
+        files[key] = global.metalsmithFiles[key];
+      });
+
+    console.log(
+      `Files associated with template change: ${Object.keys(files).length}`,
+    );
+    done();
+  };
+}
+
+module.exports = getFilesToUpdate;
