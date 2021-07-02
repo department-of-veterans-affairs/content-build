01a4504adfa78e95fe0173e862b8b273244269e5
Tim Wright <timwright12@gmail.com>
2021-07-02 16:39:58 -0400

    revert bigquery

--- src/site/stages/build/drupal/queries.js
+++ src/site/stages/build/drupal/queries.js
@@ -3,18 +3,18 @@ const queries = {
   GET_LATEST_PAGE_BY_ID: './graphql/GetLatestPageById.graphql',
 };
 
-// function getQuery(query, { useTomeSync } = {}) {
-//   if (query === queries.GET_ALL_PAGES) {
-//     // eslint-disable-next-line import/no-dynamic-require
-//     return require(query)({ useTomeSync });
-//   }
-//   // eslint-disable-next-line import/no-dynamic-require
-//   return require(query);
-// }
-function getQuery(query) {
+function getQuery(query, { useTomeSync } = {}) {
+  if (query === queries.GET_ALL_PAGES) {
+    // eslint-disable-next-line import/no-dynamic-require
+    return require(query)({ useTomeSync });
+  }
   // eslint-disable-next-line import/no-dynamic-require
   return require(query);
 }
+// function getQuery(query) {
+//   // eslint-disable-next-line import/no-dynamic-require
+//   return require(query);
+// }
 
 module.exports = {
   getQuery,
