1901d1df532f887e312839a6c6eab4364805e710
Tim Wright <timwright12@gmail.com>
2021-07-02 16:20:51 -0400

    tomesync update

--- src/site/stages/build/drupal/queries.js
+++ src/site/stages/build/drupal/queries.js
@@ -3,11 +3,15 @@ const queries = {
   GET_LATEST_PAGE_BY_ID: './graphql/GetLatestPageById.graphql',
 };
 
-function getQuery(query, { useTomeSync } = {}) {
-  if (query === queries.GET_ALL_PAGES) {
-    // eslint-disable-next-line import/no-dynamic-require
-    return require(query)({ useTomeSync });
-  }
+// function getQuery(query, { useTomeSync } = {}) {
+//   if (query === queries.GET_ALL_PAGES) {
+//     // eslint-disable-next-line import/no-dynamic-require
+//     return require(query)({ useTomeSync });
+//   }
+//   // eslint-disable-next-line import/no-dynamic-require
+//   return require(query);
+// }
+function getQuery(query) {
   // eslint-disable-next-line import/no-dynamic-require
   return require(query);
 }
