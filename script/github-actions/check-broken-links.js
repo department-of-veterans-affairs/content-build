5f73ed3374f4f0428bf8c2fc649ee600946bd17c
Oseas Moran <oseas.moran@adhocteam.us>
2021-07-02 15:48:58 -0700

    Change channel_id to public testing one

--- script/github-actions/check-broken-links.js
+++ script/github-actions/check-broken-links.js
@@ -20,7 +20,7 @@ if (fs.existsSync(reportPath)) {
     brokenLinks.brokenLinksCount > maxBrokenLinks;
   const color = shouldFail ? '#D33834' : '#FFCC00'; // danger or warning, needs to be in hex
   const summary = brokenLinks.summary;
-  const heading = `@cmshelpdesk ${brokenLinks.brokenLinksCount} broken links found in ${envName} \\n\\n <${SERVER_URL}>`;
+  const heading = `${brokenLinks.brokenLinksCount} broken links found in ${envName} \\n\\n <${SERVER_URL}>`; // TODO: Add @cmshelpdesk when prod ready
   const slackBlocks = `[{"type": "section","text": {"type": "mrkdwn","text": "${heading}"}}]`;
   const slackAttachments = `[{"mrkdwn_in": ["text"], "color": "${color}", "text": "${summary
     .replace(/\n/g, '\\n')
