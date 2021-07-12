85acbf4c099839bd27a16cda0449f205f785369b
Chris Alexander <chris.alexander@adhocteam.us>
2021-07-12 14:15:36 -0600

    Sleep for GC in addDebugInfo()

--- src/site/stages/build/add-debug-info.js
+++ src/site/stages/build/add-debug-info.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-console */
 const fs = require('fs-extra');
 const path = require('path');
+const { sleep } = require('../../../../script/utils');
 
-function addDebugInfo(files, buildtype) {
+async function addDebugInfo(files, buildtype) {
   try {
     console.log('\nAdding debug info to Drupal pages...');
 
@@ -20,56 +21,64 @@ function addDebugInfo(files, buildtype) {
       'private',
     ];
 
-    Object.keys(files)
-      .filter(fileName => files[fileName].isDrupalPage)
-      .forEach(fileName => {
-        const filePath = `build/${buildtype}/${fileName}`;
-        const tmpFilepath = `tmp/${filePath}`;
-        const tmpFileDir = path.dirname(tmpFilepath);
+    const drupalFileNames = Object.keys(files).filter(
+      fileName => files[fileName].isDrupalPage,
+    );
+    let numProcessed = 0;
 
-        if (!fs.existsSync(tmpFileDir)) {
-          fs.mkdirSync(tmpFileDir, { recursive: true });
-        }
+    for (const fileName of drupalFileNames) {
+      const filePath = `build/${buildtype}/${fileName}`;
+      const tmpFilepath = `tmp/${filePath}`;
+      const tmpFileDir = path.dirname(tmpFilepath);
 
-        const readStream = fs.createReadStream(filePath, {
-          encoding: 'utf8',
-          autoClose: true,
-        });
+      if (!fs.existsSync(tmpFileDir)) {
+        fs.mkdirSync(tmpFileDir, { recursive: true });
+      }
 
-        const outputStream = fs.createWriteStream(tmpFilepath, {
-          encoding: 'utf8',
-          autoClose: true,
-        });
-
-        const debugInfo = Object.fromEntries(
-          Object.entries(files[fileName]).filter(
-            key => !keysToIgnore.includes(key[0]),
-          ),
-        );
-
-        // `window.contentData = null` is added to Drupal pages from the debug.drupal.liquid template
-        // when the `debug` key doesn't exist in the Metalsmith file entry.
-        // We want to replace all instances of that with the debug object.
-        readStream.on('data', data => {
-          outputStream.write(
-            data
-              .toString()
-              .replace(
-                'window.contentData = null;',
-                `window.contentData = ${JSON.stringify(debugInfo)};`,
-              ),
-          );
-        });
-
-        readStream.on('end', () => {
-          outputStream.end();
-        });
-
-        outputStream.on('finish', () => {
-          // Overwrite original file with new file
-          fs.moveSync(tmpFilepath, filePath, { overwrite: true });
-        });
+      const readStream = fs.createReadStream(filePath, {
+        encoding: 'utf8',
+        autoClose: true,
       });
+
+      const outputStream = fs.createWriteStream(tmpFilepath, {
+        encoding: 'utf8',
+        autoClose: true,
+      });
+
+      const debugInfo = Object.fromEntries(
+        Object.entries(files[fileName]).filter(
+          key => !keysToIgnore.includes(key[0]),
+        ),
+      );
+
+      // `window.contentData = null` is added to Drupal pages from the debug.drupal.liquid template
+      // when the `debug` key doesn't exist in the Metalsmith file entry.
+      // We want to replace all instances of that with the debug object.
+      readStream.on('data', data => {
+        outputStream.write(
+          data
+            .toString()
+            .replace(
+              'window.contentData = null;',
+              `window.contentData = ${JSON.stringify(debugInfo)};`,
+            ),
+        );
+      });
+
+      readStream.on('end', () => {
+        outputStream.end();
+      });
+
+      outputStream.on('finish', () => {
+        // Overwrite original file with new file
+        fs.moveSync(tmpFilepath, filePath, { overwrite: true });
+      });
+
+      numProcessed++;
+      // Pause for garbage collection to free unused buffer memory
+      // eslint-disable-next-line no-await-in-loop
+      if (numProcessed % 100 === 0) await sleep(1);
+    }
   } catch (error) {
     console.error('Error adding debug info to files.\n', error);
   } finally {
