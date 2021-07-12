6ad9d3d1e8c716d827dd977983d0709cc2a3186f
Chris Alexander <chris.alexander@adhocteam.us>
2021-07-12 16:03:11 -0600

    Pause after every file when writing debug info

--- src/site/stages/build/add-debug-info.js
+++ src/site/stages/build/add-debug-info.js
@@ -3,7 +3,7 @@ const fs = require('fs-extra');
 const path = require('path');
 const { sleep } = require('../../../../script/utils');
 
-async function addDebugInfo(files, buildtype) {
+function addDebugInfo(files, buildtype) {
   try {
     console.log('\nAdding debug info to Drupal pages...');
 
@@ -21,64 +21,60 @@ async function addDebugInfo(files, buildtype) {
       'private',
     ];
 
-    const drupalFileNames = Object.keys(files).filter(
-      fileName => files[fileName].isDrupalPage,
-    );
-    let numProcessed = 0;
+    Object.keys(files)
+      .filter(fileName => files[fileName].isDrupalPage)
+      .forEach(fileName => {
+        const filePath = `build/${buildtype}/${fileName}`;
+        const tmpFilepath = `tmp/${filePath}`;
+        const tmpFileDir = path.dirname(tmpFilepath);
 
-    for (const fileName of drupalFileNames) {
-      const filePath = `build/${buildtype}/${fileName}`;
-      const tmpFilepath = `tmp/${filePath}`;
-      const tmpFileDir = path.dirname(tmpFilepath);
+        if (!fs.existsSync(tmpFileDir)) {
+          fs.mkdirSync(tmpFileDir, { recursive: true });
+        }
 
-      if (!fs.existsSync(tmpFileDir)) {
-        fs.mkdirSync(tmpFileDir, { recursive: true });
-      }
+        const readStream = fs.createReadStream(filePath, {
+          encoding: 'utf8',
+          autoClose: true,
+        });
 
-      const readStream = fs.createReadStream(filePath, {
-        encoding: 'utf8',
-        autoClose: true,
-      });
+        const outputStream = fs.createWriteStream(tmpFilepath, {
+          encoding: 'utf8',
+          autoClose: true,
+        });
 
-      const outputStream = fs.createWriteStream(tmpFilepath, {
-        encoding: 'utf8',
-        autoClose: true,
-      });
-
-      const debugInfo = Object.fromEntries(
-        Object.entries(files[fileName]).filter(
-          key => !keysToIgnore.includes(key[0]),
-        ),
-      );
-
-      // `window.contentData = null` is added to Drupal pages from the debug.drupal.liquid template
-      // when the `debug` key doesn't exist in the Metalsmith file entry.
-      // We want to replace all instances of that with the debug object.
-      readStream.on('data', data => {
-        outputStream.write(
-          data
-            .toString()
-            .replace(
-              'window.contentData = null;',
-              `window.contentData = ${JSON.stringify(debugInfo)};`,
-            ),
+        const debugInfo = Object.fromEntries(
+          Object.entries(files[fileName]).filter(
+            key => !keysToIgnore.includes(key[0]),
+          ),
         );
-      });
 
-      readStream.on('end', () => {
-        outputStream.end();
-      });
+        // `window.contentData = null` is added to Drupal pages from the debug.drupal.liquid template
+        // when the `debug` key doesn't exist in the Metalsmith file entry.
+        // We want to replace all instances of that with the debug object.
+        readStream.on('data', data => {
+          outputStream.write(
+            data
+              .toString()
+              .replace(
+                'window.contentData = null;',
+                `window.contentData = ${JSON.stringify(debugInfo)};`,
+              ),
+          );
+        });
 
-      outputStream.on('finish', () => {
-        // Overwrite original file with new file
-        fs.moveSync(tmpFilepath, filePath, { overwrite: true });
-      });
+        readStream.on('end', () => {
+          outputStream.end();
+        });
 
-      numProcessed++;
-      // Pause for garbage collection to free unused buffer memory
-      // eslint-disable-next-line no-await-in-loop
-      if (numProcessed % 100 === 0) await sleep(1);
-    }
+        outputStream.on('finish', async () => {
+          // Overwrite original file with new file
+          fs.moveSync(tmpFilepath, filePath, { overwrite: true });
+          console.log(`Completed ${filePath}`);
+
+          // Pause for garbage collection to free unused buffer memory
+          await sleep(1);
+        });
+      });
   } catch (error) {
     console.error('Error adding debug info to files.\n', error);
   } finally {
