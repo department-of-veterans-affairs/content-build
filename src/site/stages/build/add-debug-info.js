07f47188588bdb1b7142532f537070b7f6997905
Chris Alexander <chris.alexander@adhocteam.us>
2021-07-13 11:42:23 -0600

    Limit number of open files

--- src/site/stages/build/add-debug-info.js
+++ src/site/stages/build/add-debug-info.js
@@ -1,64 +1,70 @@
-/* eslint-disable no-console */
+/* eslint-disable no-console, no-await-in-loop */
 const fs = require('fs-extra');
-const path = require('path');
+const _ = require('lodash');
 const { sleep } = require('../../../../script/utils');
+
+const NUM_CONCURRENT_FILES = 10;
+
+const keysToIgnore = [
+  'breadcrumb_path',
+  'collection',
+  'contents',
+  'filename',
+  'isDrupalPage',
+  'layout',
+  'modified',
+  'nav_children',
+  'nav_path',
+  'path',
+  'private',
+];
+
+async function writeFile(fileName, fileObject, buildtype) {
+  if (!fileName) return;
+
+  const filePath = `build/${buildtype}/${fileName}`;
+  console.log(`file ${fileName}`);
+  const originalContents = await fs.readFileSync(filePath, 'utf8');
+  console.log('+ read file');
+  const debugInfo = _.omit(fileObject, keysToIgnore);
+
+  // `window.contentData = null` is added to Drupal pages from the debug.drupal.liquid template
+  // when the `debug` key doesn't exist in the Metalsmith file entry.
+  // We want to replace all instances of that with the debug object.
+  const oldString = 'window.contentData = null;';
+  const newString = `window.contentData = ${JSON.stringify(debugInfo)};`;
+  const newContents = originalContents.toString().replace(oldString, newString);
+  console.log('+ replaced contents');
+
+  await fs.writeFileSync(filePath, newContents, { overwrite: true });
+  console.log(`+ wrote file ${filePath}`);
+}
 
 async function addDebugInfo(files, buildtype) {
   try {
     console.log('\nAdding debug info to Drupal pages...\n');
-
-    const keysToIgnore = [
-      'breadcrumb_path',
-      'collection',
-      'contents',
-      'filename',
-      'isDrupalPage',
-      'layout',
-      'modified',
-      'nav_children',
-      'nav_path',
-      'path',
-      'private',
-    ];
-
     const drupalFileNames = Object.keys(files).filter(
       fileName => files[fileName].isDrupalPage,
     );
 
-    for (const fileName of drupalFileNames) {
-      const filePath = `build/${buildtype}/${fileName}`;
-      const tmpFilepath = `tmp/${filePath}`;
-      const tmpFileDir = path.dirname(tmpFilepath);
-
-      if (!fs.existsSync(tmpFileDir)) {
-        fs.mkdirSync(tmpFileDir, { recursive: true });
+    console.log('Looping through drupal filenames...');
+    // Limit the number of simultaneous open files with an array of promises.
+    // This also limits peak memory use.
+    while (drupalFileNames.length) {
+      const promises = [];
+      for (let i = 1; i <= NUM_CONCURRENT_FILES; i++) {
+        const fileName = drupalFileNames.pop();
+        promises.push(writeFile(fileName, files[fileName], buildtype));
       }
-
-      const debugInfo = Object.fromEntries(
-        Object.entries(files[fileName]).filter(
-          key => !keysToIgnore.includes(key[0]),
-        ),
-      );
-      const oldString = 'window.contentData = null;';
-      const newString = `window.contentData = ${JSON.stringify(debugInfo)};`;
-
-      /* eslint-disable-next-line no-await-in-loop */
-      const originalContents = await fs.readFileSync(filePath, 'utf8');
-      const newContents = originalContents
-        .toString()
-        .replace(oldString, newString);
-
-      /* eslint-disable-next-line no-await-in-loop */
-      await fs.writeFileSync(filePath, newContents, { overwrite: true });
-
-      console.log(`wrote file ${filePath}`);
+      await Promise.all(promises);
 
       // Pause for garbage collection to free unused buffer memory
-      // eslint-disable-next-line no-await-in-loop
       await sleep(1);
     }
   } catch (error) {
-    console.error('\nError adding debug info to files.\n', error);
+    console.error('Error adding debug info to files.\n', error);
+  } finally {
+    console.log('Finished adding debug info to Drupal pages.\n');
   }
 }
 
