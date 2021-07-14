835f8f640afa22ec464c4a54b7e3dd2a7d2d18ec
Gaida Escobar <84030819+gescobardev@users.noreply.github.com>
2021-07-14 14:39:59 -0400

    Added vet center title to metatags (#346)
    * added unit tests to metatags.drupal.liquid
    
    * merge minor changes
    
    * added tests
    
    * minor changes
    
    Co-authored-by: Gaida Escobar <gaidaescobar@gescobar-C02Z18JJLVCG.fios-router.home>

---
+++ src/site/layouts/tests/vet_center/vet_center_locations_list.drupal.unit.spec.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+import { beforeEach } from 'mocha';
+import { parseFixture, renderHTML } from '~/site/tests/support';
+
+const layoutPath = 'src/site/layouts/vet_center_locations_list.drupal.liquid';
+
+describe('Vet Center Locations Page', () => {
+  let container;
+  const data = parseFixture(
+    'src/site/layouts/tests/vet_center/fixtures/vet_center_escanaba_data.json',
+  );
+
+  beforeEach(async () => {
+    container = await renderHTML(layoutPath, data);
+  });
+
+  it('renders vet center name in metatag - [Vet Center] | Veterans Affairs', () => {
+    expect(
+      container
+        .querySelector("meta[property='og:title']")
+        .getAttribute('content'),
+    ).to.equal('Escanaba Vet Center | Veterans Affairs');
+  });
+
+  it('renders vet center name in title-tag - [Vet Center] | Veterans Affairs', () => {
+    expect(container.querySelector('title').innerHTML).to.equal(
+      'Escanaba Vet Center | Veterans Affairs',
+    );
+  });
+});
