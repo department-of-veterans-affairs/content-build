770bb3ebd343ba8db8308eb4a3c7a0b49d239d90
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2021-07-19 16:32:56 -0400

    Show social links on local facility pages (#379)

--- src/site/stages/build/drupal/graphql/facilities-fragments/healthCareSocialMedia.fields.graphql.js
+++ src/site/stages/build/drupal/graphql/facilities-fragments/healthCareSocialMedia.fields.graphql.js
@@ -2,4 +2,20 @@
  * Social media fields on health care systems and health care facilities
  */
 module.exports = `
+    fieldFacebook {
+      title
+      uri
+    }    
+    fieldTwitter {
+      title    
+      uri
+    }
+    fieldFlickr {
+      title    
+      uri
+    }
+    fieldInstagram {
+      title    
+      uri
+    }
 `;
