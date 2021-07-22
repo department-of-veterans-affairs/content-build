e3dfa029b3031716b8909d30a3e0eaa53af10654
Ryan Leahy <ryan.leahy@adhocteam.us>
2021-07-22 16:23:44 -0400

    Combine metatags templates into one

--- src/site/includes/tests/metatags.drupal.unit.spec.js
+++ src/site/includes/tests/metatags.drupal.unit.spec.js
@@ -88,4 +88,142 @@ describe('Metatags', () => {
       );
     });
   });
+
+  describe('legacy metatags.liquid', () => {
+    it('returns metatag - [og:site_name] | Veterans Affairs when drupalTags is included in content', async () => {
+      container = await renderHTML(layoutPath, {
+        drupalTags: true,
+      });
+
+      expect(
+        container
+          .querySelector("meta[property='og:site_name']")
+          .getAttribute('content'),
+      ).to.equal('Veterans Affairs');
+    });
+
+    it('returns metatag - [og:site_name] | VA.gov when drupalTags is not included in content', async () => {
+      container = await renderHTML(layoutPath, {
+        drupalTags: false,
+      });
+
+      expect(
+        container
+          .querySelector("meta[property='og:site_name']")
+          .getAttribute('content'),
+      ).to.equal('VA.gov');
+    });
+
+    it('returns metatag - [DC.Date] | {date} when lastupdate is included', async () => {
+      container = await renderHTML(layoutPath, {
+        lastupdate: 'January 01, 2021',
+      });
+
+      expect(
+        container.querySelector("meta[name='DC.Date']").getAttribute('content'),
+      ).to.equal('2021-01-01');
+    });
+
+    it('returns metatag - [DC.Date] | {date} when changed is included', async () => {
+      container = await renderHTML(layoutPath, {
+        changed: '1612094400',
+      });
+
+      expect(
+        container.querySelector("meta[name='DC.Date']").getAttribute('content'),
+      ).to.equal('2021-01-31');
+    });
+
+    it('returns metatag - [og:url] | {host}{path} when entityUrl.path is included', async () => {
+      container = await renderHTML(layoutPath, {
+        entityUrl: {
+          path: '/test-url',
+        },
+      });
+
+      expect(
+        container
+          .querySelector("meta[property='og:url']")
+          .getAttribute('content'),
+      ).to.equal('https://dev.va.gov/test-url/');
+    });
+
+    it('returns metatag - [og:url] | {host}/{path} when entityUrl.path is not included', async () => {
+      container = await renderHTML(layoutPath, {
+        path: 'example-url',
+      });
+
+      expect(
+        container
+          .querySelector("meta[property='og:url']")
+          .getAttribute('content'),
+      ).to.equal('https://dev.va.gov/example-url');
+    });
+
+    it('returns metatag - [twitter:title] | MetaTitle | Veterans Affairs when title is included', async () => {
+      container = await renderHTML(layoutPath, {
+        title: 'metaTitle',
+      });
+
+      expect(
+        container
+          .querySelector("meta[name='twitter:title']")
+          .getAttribute('content'),
+      ).to.equal('MetaTitle | Veterans Affairs');
+    });
+
+    it('returns metatag - [twitter:title] | {host} when title is not included', async () => {
+      container = await renderHTML(layoutPath, {});
+
+      expect(
+        container
+          .querySelector("meta[name='twitter:title']")
+          .getAttribute('content'),
+      ).to.equal('https://dev.va.gov');
+    });
+
+    it('returns metatag - [keywords] | veterans when keywords is provided', async () => {
+      container = await renderHTML(layoutPath, {
+        keywords: 'veterans',
+      });
+
+      expect(
+        container
+          .querySelector("meta[name='keywords']")
+          .getAttribute('content'),
+      ).to.equal('veterans');
+    });
+
+    it('returns title VA.gov when title == "HOME', async () => {
+      container = await renderHTML(layoutPath, {
+        title: 'Home',
+      });
+
+      expect(container.querySelector('title').text).to.equal('VA.gov');
+    });
+
+    it('returns title {metaTitle} when title != "HOME', async () => {
+      container = await renderHTML(layoutPath, {
+        title: 'Search',
+      });
+
+      expect(container.querySelector('title').text).to.equal(
+        'Search | Veterans Affairs',
+      );
+    });
+  });
+
+  describe('tags', () => {
+    it('includes meta tags for each item in tags when provided', async () => {
+      container = await renderHTML(layoutPath, {
+        tags: ['foo'],
+      });
+
+      expect(
+        container
+          .querySelector("meta[property='article:tag']")
+          .getAttribute('content'),
+      ).to.equal('foo');
+    });
+  });
 });
