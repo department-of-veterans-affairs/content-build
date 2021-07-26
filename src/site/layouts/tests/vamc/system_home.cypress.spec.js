62553626e961f26debae9c5e2a2ab7088c8d7f5a
Gaida Escobar <gaidaescobar@gescobar-C02Z18JJLVCG.fios-router.home>
2021-07-26 13:55:19 -0400

    cleaned up code

--- src/site/layouts/tests/vamc/system_home.cypress.spec.js
+++ src/site/layouts/tests/vamc/system_home.cypress.spec.js
@@ -20,13 +20,12 @@ Cypress.Commands.add('checkElements', (page, isMobile) => {
     cy.get('#sidenav-menu').should('not.be.visible');
   }
   cy.get('h1').contains('VA Pittsburgh health care');
-  cy.get('h2').contains('Locations');
   cy.get('.system-img')
     .should('be.visible')
     .and($img => {
-      // "naturalWidth" and "naturalHeight" are set when the image loads
       expect($img[0].naturalWidth).to.be.greaterThan(0);
     });
+  cy.get('h2').contains('Locations');
   cy.get('[data-template="includes/facilityListing"]').each($listing => {
     cy.wrap($listing)
       .find('address')
