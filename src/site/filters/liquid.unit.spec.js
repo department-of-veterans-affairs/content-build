2159b0c23d00ba93471d8ca5615b984683d65294
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2021-07-27 15:25:21 -0400

    Wrap phone numbers with parentheses around the area code

--- src/site/filters/liquid.unit.spec.js
+++ src/site/filters/liquid.unit.spec.js
@@ -1131,6 +1131,13 @@ describe('phoneLinks', () => {
     expect(liquid.filters.phoneLinks(text)).to.equal(expected);
   });
 
+  it('wraps phone numbers with parentheses around the area code', () => {
+    const text = 'Here is a phone number: (123)-456-7890. Pretty cool!';
+    const expected =
+      'Here is a phone number: <a target="_blank" href="tel:123-456-7890">123-456-7890</a>. Pretty cool!';
+    expect(liquid.filters.phoneLinks(text)).to.equal(expected);
+  });
+
   it('does not double-wrap phone numbers', () => {
     const html =
       'Here is a <a href="test">phone number</a>: <a target="_blank" href="tel:123-456-7890">123-456-7890</a>. Pretty cool!';
