28cd158584af10c9fb8e1dd323b15e2c693ee004
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2021-08-09 09:47:09 -0400

    Sort satellite vet centers (#421)
    * Sort satellite vet centers
    
    * Change fixture name
    
    * Fix spelling error
    
    * Adjust logic in vet_center_locations_list.drupal.liquid to include mobile vet centers found in reverseFieldOfficeNode
    
    Add/fix unit tests
    
    * Add null check to getValuesForKey
    
    * Added a test for rejectBy and cleaned up test for filterBy
    
    * Fix test wording

--- src/site/layouts/tests/vet_center/vet_center_locations_list.drupal.unit.spec.js
+++ src/site/layouts/tests/vet_center/vet_center_locations_list.drupal.unit.spec.js
@@ -7,7 +7,7 @@ const layoutPath = 'src/site/layouts/vet_center_locations_list.drupal.liquid';
 describe('Vet Center Locations Page', () => {
   let container;
   const data = parseFixture(
-    'src/site/layouts/tests/vet_center/fixtures/vet_center_escanaba_data.json',
+    'src/site/layouts/tests/vet_center/fixtures/vet_center_locations_data.json',
   );
 
   beforeEach(async () => {
@@ -19,12 +19,24 @@ describe('Vet Center Locations Page', () => {
       container
         .querySelector("meta[property='og:title']")
         .getAttribute('content'),
-    ).to.equal('Escanaba Vet Center | Veterans Affairs');
+    ).to.equal('Locations | Nashville Vet Center | Veterans Affairs');
   });
 
   it('renders vet center name in title-tag - [Vet Center] | Veterans Affairs', () => {
     expect(container.querySelector('title').innerHTML).to.equal(
-      'Escanaba Vet Center | Veterans Affairs',
+      'Locations | Nashville Vet Center | Veterans Affairs',
     );
+  });
+
+  it('sorts satellite locations by title', () => {
+    expect(
+      Array.from(container.querySelectorAll('vctitle')).map(el => el.innerHTML),
+    ).to.deep.equal([
+      'Nashville Vet Center', // main location
+      'Clarksville Outstation',
+      'Nashville Vet Center - Bowling Green',
+      'Knoxville Mobile Vet Center',
+      'Memphis Mobile Vet Center',
+    ]);
   });
 });
