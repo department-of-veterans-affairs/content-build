8ceb90e8afdb9f4a2ef0bf7f22b645cd0c6ae14f
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2021-08-25 10:10:52 -0400

    Additional event listing page unit tests (#474)

--- src/site/layouts/tests/event_listing/event_listing.unit.spec.js
+++ src/site/layouts/tests/event_listing/event_listing.unit.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { renderHTML } from '~/site/tests/support';
+import { renderHTML, parseFixture } from '~/site/tests/support';
 import axeCheck from '~/site/tests/support/axe';
 import { readFileSync } from 'fs';
 import path from 'path';
@@ -29,67 +29,92 @@ const dateStringLookup = {
 };
 
 describe('Events Listing Page', () => {
-  let data;
-  const fixturePath = 'tests/event_listing/fixtures/event-listing-data.json';
+  describe('with events', () => {
+    let data;
+    const fixturePath = 'tests/event_listing/fixtures/event-listing-data.json';
 
-  try {
-    data = JSON.parse(getFile(fixturePath));
-  } catch (error) {
-    /* eslint-disable no-console */
-    console.log(`Error parsing JSON fixture in:\n`, error);
-    /* eslint-enable no-console */
-  }
+    try {
+      data = JSON.parse(getFile(fixturePath));
+    } catch (error) {
+      /* eslint-disable no-console */
+      console.log(`Error parsing JSON fixture in:\n`, error);
+      /* eslint-enable no-console */
+    }
 
-  data.pastEvents.entities.forEach((event, index) => {
-    data.pastEvents.entities[index].fieldDatetimeRangeTimezone.value =
-      dateStringLookup[event.fieldDatetimeRangeTimezone.value];
-    data.pastEvents.entities[index].fieldDatetimeRangeTimezone.endValue =
-      dateStringLookup[event.fieldDatetimeRangeTimezone.endValue];
+    data.pastEvents.entities.forEach((event, index) => {
+      data.pastEvents.entities[index].fieldDatetimeRangeTimezone.value =
+        dateStringLookup[event.fieldDatetimeRangeTimezone.value];
+      data.pastEvents.entities[index].fieldDatetimeRangeTimezone.endValue =
+        dateStringLookup[event.fieldDatetimeRangeTimezone.endValue];
+    });
+
+    data.reverseFieldListingNode.entities.forEach((event, index) => {
+      data.reverseFieldListingNode.entities[
+        index
+      ].fieldDatetimeRangeTimezone.value =
+        dateStringLookup[event.fieldDatetimeRangeTimezone.value];
+      data.reverseFieldListingNode.entities[
+        index
+      ].fieldDatetimeRangeTimezone.endValue =
+        dateStringLookup[event.fieldDatetimeRangeTimezone.endValue];
+    });
+
+    let container;
+
+    before(async () => {
+      container = await renderHTML(layoutPath, data);
+    });
+
+    it('reports no axe violations', async () => {
+      const violations = await axeCheck(container);
+      expect(violations.length).to.equal(0);
+    });
+
+    it('does not render "no upcoming events" message"', async () => {
+      expect(container.getElementById('no-events-message')).not.to.exist;
+    });
+
+    it('renders upcoming/past events toggle', async () => {
+      expect(container.getElementById('events-list-toggle')).to.exist;
+    });
+
+    it('renders correct spotlight event', async () => {
+      expect(container.querySelector('#featured-content a').innerHTML).to.equal(
+        'Virtual Veterans Town Hall',
+      );
+    });
+
+    it('renders correct upcoming event', async () => {
+      expect(
+        container.querySelector('#virtual-women-veterans-health- a').innerHTML,
+      ).to.equal('Virtual Women Veterans Health Public Forum');
+    });
+
+    it('does not render featured event twice', () => {
+      expect(
+        Array.from(container.querySelectorAll('h2 a')).filter(e => {
+          return e.text === 'Virtual Veterans Town Hall';
+        }).length,
+      ).to.equal(1);
+    });
   });
 
-  data.reverseFieldListingNode.entities.forEach((event, index) => {
-    data.reverseFieldListingNode.entities[
-      index
-    ].fieldDatetimeRangeTimezone.value =
-      dateStringLookup[event.fieldDatetimeRangeTimezone.value];
-    data.reverseFieldListingNode.entities[
-      index
-    ].fieldDatetimeRangeTimezone.endValue =
-      dateStringLookup[event.fieldDatetimeRangeTimezone.endValue];
-  });
-
-  let container;
-
-  before(async () => {
-    container = await renderHTML(layoutPath, data);
-  });
-
-  it('reports no axe violations', async () => {
-    const violations = await axeCheck(container);
-    expect(violations.length).to.equal(0);
-  });
-
-  it('renders upcoming/past events toggle', async () => {
-    expect(container.getElementById('events-list-toggle')).to.exist;
-  });
-
-  it('renders correct spotlight event', async () => {
-    expect(container.querySelector('#featured-content a').innerHTML).to.equal(
-      'Virtual Veterans Town Hall',
+  describe('without events', () => {
+    const data = parseFixture(
+      'src/site/layouts/tests/event_listing/fixtures/event-listing-data-no-events.json',
     );
-  });
+    let container;
 
-  it('renders correct upcoming event', async () => {
-    expect(
-      container.querySelector('#virtual-women-veterans-health- a').innerHTML,
-    ).to.equal('Virtual Women Veterans Health Public Forum');
-  });
+    before(async () => {
+      container = await renderHTML(layoutPath, data, 'noEvents');
+    });
 
-  it('does not render featured event twice', () => {
-    expect(
-      Array.from(container.querySelectorAll('h2 a')).filter(e => {
-        return e.text === 'Virtual Veterans Town Hall';
-      }).length,
-    ).to.equal(1);
+    it('does not render upcoming/past events toggle', async () => {
+      expect(container.getElementById('events-list-toggle')).not.to.exist;
+    });
+
+    it('renders "no upcoming events" message"', async () => {
+      expect(container.getElementById('no-events-message')).to.exist;
+    });
   });
 });
