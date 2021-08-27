e1334a3403e75ef475d233c615d0afddf66a6278
Ryan Leahy <ryan.leahy@adhocteam.us>
2021-08-27 16:04:29 -0400

    Write metalsmith output to src/site/tests/html

--- src/site/stages/build/plugins/modify-dom/tests/add-id-to-subheadings.spec.js
+++ src/site/stages/build/plugins/modify-dom/tests/add-id-to-subheadings.spec.js
@@ -1,31 +1,17 @@
-import fs from 'fs';
-import path from 'path';
 import { expect } from 'chai';
-import modifyDomPlugin from './modifyDomPlugin';
 import addSubheadingsIds from '../add-id-to-subheadings';
-import Metalsmith from 'metalsmith';
-import { JSDOM } from 'jsdom';
-
-const metalsmith = Metalsmith(
-  './src/site/stages/build/plugins/modify-dom/tests/fixtures/',
-);
-
-after(() => {
-  fs.rmdirSync(path.join(__dirname, './fixtures/build'), {
-    recursive: true,
-    force: true,
-  });
-});
+import { testMetalsmithPlugin } from '~/site/tests/support';
 
 describe('addIdToSubheadings', () => {
   it('should add <h2> ids to "On this Page" table of contents', done => {
-    metalsmith
-      .use(modifyDomPlugin([addSubheadingsIds]))
-      .build(function(err, files) {
-        const { document } = new JSDOM(
-          files['testSubheadings.html'].contents.toString(),
-        ).window;
-
+    testMetalsmithPlugin(
+      {
+        fileName: 'testSubheadings.html',
+        fixturesPath:
+          './src/site/stages/build/plugins/modify-dom/tests/fixtures/',
+        plugins: [addSubheadingsIds],
+      },
+      document => {
         expect(
           document
             .querySelector('#table-of-contents li a[href="#test-h2"]')
@@ -35,24 +21,29 @@ describe('addIdToSubheadings', () => {
         expect(
           document.querySelector('#table-of-contents li a[href="#test-h3"]'),
         ).to.eq(null);
+
         done();
-      });
+      },
+    );
   });
 
   it('should not add an h2 to the table of contents if it is inside an alert', done => {
-    metalsmith
-      .use(modifyDomPlugin([addSubheadingsIds]))
-      .build(function(err, files) {
-        const { document } = new JSDOM(
-          files['testSubheadings.html'].contents.toString(),
-        ).window;
-
+    testMetalsmithPlugin(
+      {
+        fileName: 'testSubheadings.html',
+        fixturesPath:
+          './src/site/stages/build/plugins/modify-dom/tests/fixtures/',
+        plugins: [addSubheadingsIds],
+      },
+      document => {
         expect(
           document.querySelector(
             '#table-of-contents li a[href="#test-alert-h2"]',
           ),
         ).to.eq(null);
+
         done();
-      });
+      },
+    );
   });
 });
