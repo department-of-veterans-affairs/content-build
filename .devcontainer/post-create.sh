ca2192acb8119c2c02d0cae7017fa8c28b3a49dc
Tim Wright <timwright12@gmail.com>
2021-10-05 16:45:48 -0400

    Initial codespace setup

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# configure NVM
+printf "\n\n#####" Configuring Node, NPM, and Yarn #####\n""
+source $NVM_DIR/nvm.sh
+nvm install || true # ignore exit code due to npm prefix
+nvm use --delete-prefix
+
+# check versions
+printf "Node version: "
+node --version
+
+printf "Yarn version: "
+yarn --version
+
+# download content repo
+printf "\n\n##### Downloading content repo #####\n"
+yarn install-repos
