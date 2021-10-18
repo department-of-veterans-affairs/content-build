2bcf220b96ac41500c893e250e82c2423688c66d
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-10-18 11:41:33 -0500

    Remove unused Cypress environment variable (#659)
    * Remove unused Cypress environment variable
    
    * Fix

--- jenkins/common.groovy
+++ jenkins/common.groovy
@@ -241,14 +241,14 @@ def integrationTests(dockerContainer, ref) {
             parallel (
               failFast: true,
               cypress: {
-                sh "export IMAGE_TAG=${IMAGE_TAG} && docker-compose -p cypress-${env.EXECUTOR_NUMBER} up -d && docker-compose -p cypress-${env.EXECUTOR_NUMBER} run --rm --entrypoint=npm -e CI=true -e NO_COLOR=1 content-build --no-color run cy:test:docker"
+                sh "export IMAGE_TAG=${IMAGE_TAG} && docker-compose -p cypress-${env.EXECUTOR_NUMBER} up -d && docker-compose -p cypress-${env.EXECUTOR_NUMBER} run --rm --entrypoint=npm -e NO_COLOR=1 content-build --no-color run cy:test:docker"
               }
             )
           } else {
             parallel (
               failFast: true,
               cypress: {
-                sh "export IMAGE_TAG=${IMAGE_TAG} && docker-compose -p cypress-${env.EXECUTOR_NUMBER} up -d && docker-compose -p cypress-${env.EXECUTOR_NUMBER} run --rm --entrypoint=npm -e CI=true -e NO_COLOR=1 content-build --no-color run cy:test:docker"
+                sh "export IMAGE_TAG=${IMAGE_TAG} && docker-compose -p cypress-${env.EXECUTOR_NUMBER} up -d && docker-compose -p cypress-${env.EXECUTOR_NUMBER} run --rm --entrypoint=npm -e NO_COLOR=1 content-build --no-color run cy:test:docker"
               }
             )
           }
