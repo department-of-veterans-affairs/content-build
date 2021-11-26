1142ce0b7c92c399c9b9d1cf00c8b5f17c634893
Kelson Adams <kadams@governmentcio.com>
2021-11-26 11:10:07 -0700

    Remove unused e2e tests + fixture

--- src/site/layouts/tests/vamc/e2e/commands.cypress.js
+++
@@ -1,7 +0,0 @@
-Cypress.Commands.add('checkSideNav', isMobile => {
-  if (isMobile) {
-    cy.get('#sidenav-menu').should('be.visible');
-  } else {
-    cy.get('#sidenav-menu').should('not.be.visible');
-  }
-});
