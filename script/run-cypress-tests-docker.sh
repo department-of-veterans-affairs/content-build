67567bd683ee2546fc940a136c33d7b0d2fb7ffe
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-12-17 08:36:04 -0600

    Remove script used for Cypress tests on Jenkins

--- script/run-cypress-tests-docker.sh
+++
@@ -1,7 +0,0 @@
-if [ "$(find src -name '*.cypress.spec.js' | wc -l)" -eq 0 ]; then
-  echo "No Cypress tests found."
-  exit 0
-else
-  export CYPRESS_BASE_URL=http://content-build:3001
-  yarn cy:run --env buildtype=vagovprod
-fi
