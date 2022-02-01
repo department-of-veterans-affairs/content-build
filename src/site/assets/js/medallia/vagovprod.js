418828fa75784e04d26d92146d03ed94edccab78
aubreyarcangel <aarcangel@gmail.com>
2022-02-01 09:49:12 -0500

    react feedback full-width and added dynamic survey numbers

--- src/site/assets/js/medallia/vagovprod.js
+++ src/site/assets/js/medallia/vagovprod.js
@@ -1,4 +1,4 @@
-// Medallia Embed Script
+// Medallia intercept survey for Teamsite pages on va.gov/ORMDI/*.*
 (function(g) {
   var isStaging = window.location.host.includes('staging');
   if (!isStaging) return;
@@ -45,3 +45,49 @@
       g[aT].push(arguments);
     });
 })(window);
+
+
+if (window.KAMPYLE_ONSITE_SDK) {
+  //Check Medallia code has been loaded 
+  onsiteLoaded();
+  //your custom function
+} else {
+  // On the neb_OnsiteLoaded event call onsiteLoaded function 
+  window.addEventListener('neb_OnsiteLoaded', onsiteLoaded);
+}
+
+function onsiteLoaded() {
+  // load the form and store status (true/false) in neb_status
+  var neb_status = KAMPYLE_ONSITE_SDK.loadForm(getSurveyNumber());
+  // if (neb_status === true) {
+    // if form is loaded then decide what to do  
+    // your code goes here
+  // }
+}
+//  show medallia
+if (window.KAMPYLE_ONSITE_SDK) {
+  //If Medallia code has been loaded z
+  onsiteLoaded(); //your custom function
+} else {
+  // On the neb_OnsiteLoaded event, call onsiteLoaded function
+  window.addEventListener('neb_OnsiteLoaded', onsiteLoaded);
+}
+
+function onsiteLoaded() {
+  // load the form and store status (true/false) in neb_status
+  var neb_status = KAMPYLE_ONSITE_SDK.loadForm(getSurveyNumber());
+  // If form is loaded
+  if (neb_status === true) {
+    // set CSS attribute display to inherit so button can be seen
+    document.getElementById("mdFormButton").style.display = "inherit"
+  }
+}
+
+function getSurveyNumber() {
+  var pathname = window.location.pathname;
+  if (pathname === '/search/' || pathname === '/search/') {
+    return 21
+  } else {
+    return 17
+  }
+}
--- src/site/assets/js/medallia/vagovstaging.js
+++ src/site/assets/js/medallia/vagovstaging.js
@@ -12,7 +12,7 @@
 
   function onsiteLoaded() {
     // load the form and store status (true/false) in neb_status
-    var neb_status = KAMPYLE_ONSITE_SDK.loadForm(4);
+    var neb_status = KAMPYLE_ONSITE_SDK.loadForm(getSurveyNumber());
     // if (neb_status === true) {
       // if form is loaded then decide what to do  
       // your code goes here
@@ -31,10 +31,25 @@
 
   function onsiteLoaded() {
     // load the form and store status (true/false) in neb_status
-    var neb_status = KAMPYLE_ONSITE_SDK.loadForm(4);
+    var neb_status = KAMPYLE_ONSITE_SDK.loadForm(getSurveyNumber());
     // If form is loaded
-    // if (neb_status === true) {
-    //   // set CSS attribute display to inherit so button can be seen
-    //   document.getElementById("mdFormButton").style.display = "inherit"
-    // }
+    if (neb_status === true) {
+      // set CSS attribute display to inherit so button can be seen
+      document.getElementById("mdFormButton").style.display = "inherit"
+    }
   }
+
+console.log('this is the staging js file:', window.location.pathname)
+  
+function getSurveyNumber() {
+  var pathname = window.location.pathname;
+    if (pathname === '/search/') {
+      console.log('returning survey: 20')
+    return 20
+    } else {
+        console.log('returning survey: 11')
+    return 11
+  }
+}
+
+console.log('getSurveyNumber():', getSurveyNumber())
