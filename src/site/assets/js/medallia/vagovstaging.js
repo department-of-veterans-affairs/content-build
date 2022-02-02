a11b49f3a4bfd1c8923b5520f2ed81584fc98f41
aubreyarcangel <aarcangel@gmail.com>
2022-02-01 22:01:40 -0500

    added helpers

--- src/site/assets/js/medallia/vagovstaging.js
+++ src/site/assets/js/medallia/vagovstaging.js
@@ -1,55 +1,58 @@
   
-// load medallia
-
   if (window.KAMPYLE_ONSITE_SDK) {
-    //Check Medallia code has been loaded 
     onsiteLoaded();
-    //your custom function
   } else {
-    // On the neb_OnsiteLoaded event call onsiteLoaded function 
     window.addEventListener('neb_OnsiteLoaded', onsiteLoaded);
   }
 
   function onsiteLoaded() {
     // load the form and store status (true/false) in neb_status
-    var neb_status = KAMPYLE_ONSITE_SDK.loadForm(getSurveyNumber());
-    // if (neb_status === true) {
+    var neb_status = KAMPYLE_ONSITE_SDK.loadForm(getSurveyNumber(window.location.pathname));
+      if (neb_status === true) {
+        console.log(`the form has loaded ${getSurveyNumber(window.location.pathname)} form`)
       // if form is loaded then decide what to do  
       // your code goes here
-    // }
-  }
-
-//  show medallia
-
-  if (window.KAMPYLE_ONSITE_SDK) {
-    //If Medallia code has been loaded 
-    onsiteLoaded(); //your custom function
-  } else {
-    // On the neb_OnsiteLoaded event, call onsiteLoaded function
-    window.addEventListener('neb_OnsiteLoaded', onsiteLoaded);
-  }
-
-  function onsiteLoaded() {
-    // load the form and store status (true/false) in neb_status
-    var neb_status = KAMPYLE_ONSITE_SDK.loadForm(getSurveyNumber());
-    // If form is loaded
-    if (neb_status === true) {
-      // set CSS attribute display to inherit so button can be seen
-      document.getElementById("mdFormButton").style.display = "inherit"
     }
   }
 
-console.log('this is the staging js file:', window.location.pathname)
-  
-function getSurveyNumber() {
-  var pathname = window.location.pathname;
-    if (pathname === '/search/') {
-      console.log('returning survey: 20')
-    return 20
+function getSurveyNumber(url) {
+    let pathurl = trimSlash(url)
+
+    if (vagovstagingsurveys[pathurl]) {
+        console.log('returning in getSurveyNumberhelper: ', stagingsurveys[pathurl]);
+        return vagovstagingsurveys[pathurl];
     } else {
-        console.log('returning survey: 11')
-    return 11
-  }
+        console.log('not on /search. pathurl is: ', pathurl);
+        return 11;
+    }
 }
 
-console.log('getSurveyNumber():', getSurveyNumber())
+const vagovstagingsurveys = {
+    "/search": 20
+}
+
+// const pathname = trimEnd(window.location.pathname, '/')
+function trimSlash(url) {
+    if (url.charAt(url.length - 1) === '/') {
+      console.log('trimmed slash')  
+      return url.slice(0,url.length-1);
+    } else {
+        console.log('did not trim slash')
+        return url;
+    }
+}
+
+// console.log('this is the vagovstaging js file:', window.location.pathname)
+  
+// function getSurveyNumber() {
+//   var pathname = window.location.pathname;
+//     if (pathname === '/search/') {
+//       console.log('returning survey: 20')
+//     return 20
+//     } else {
+//         console.log('returning survey: 11')
+//     return 11
+//   }
+// }
+
+// console.log('getSurveyNumber():', getSurveyNumber())
