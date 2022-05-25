48a52e26ca45eaa156a36fd95c0eaf1416b7fe85
Max Antonucci <maxx1128@gmail.com>
2022-05-25 13:42:43 -0400

    VA-8991: Remove unneeded Ohio logic in Cerner filter

--- src/site/filters/liquid.js
+++ src/site/filters/liquid.js
@@ -1302,29 +1302,27 @@ module.exports = function registerFilters() {
     return sidebarData;
   };
 
-  liquid.filters.topTaskUrl = (flag, path, systemName) => {
+  liquid.filters.topTaskUrl = (flag, path) => {
     if (flag === 'cerner' && path === 'refill-track-prescriptions/') {
       return 'https://patientportal.myhealth.va.gov/pages/medications/current';
     }
+
     if (flag === 'cerner' && path === 'secure-messaging/') {
       return 'https://patientportal.myhealth.va.gov/pages/messaging/inbox';
     }
+
     if (flag === 'cerner' && path === 'schedule-view-va-appointments/') {
       return 'https://patientportal.myhealth.va.gov/pages/scheduling/upcoming';
     }
+
     if (flag === 'cerner' && path === 'get-medical-records/') {
       return 'https://patientportal.myhealth.va.gov/pages/health_record/clinical_documents/open_notes?pagelet=https%3A%2F%2Fportal.myhealth.va.gov%2Fperson%2F1056308125V679416%2Fhealth-record%2Fopen-notes';
     }
+
     if (flag === 'cerner' && path === 'view-test-and-lab-results/') {
       return 'https://patientportal.myhealth.va.gov/pages/health_record/results';
     }
-    if (
-      flag === 'cerner' ||
-      (systemName === 'VA Central Ohio health care' &&
-        path === 'schedule-view-va-appointments/')
-    ) {
-      return 'https://patientportal.myhealth.va.gov';
-    }
+
     return `/health-care/${path}`;
   };
 
