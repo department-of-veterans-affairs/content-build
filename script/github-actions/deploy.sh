0dc4bf4c3f692bea7c6d8a5d3c93823a0e8c773f
Theo Bentum <theo@adhocteam.us>
2022-06-02 14:19:58 -0700

    Update Deploy Script (#1156)
    * Remove last include
    
    * Add quotes around glob

--- script/github-actions/deploy.sh
+++ script/github-actions/deploy.sh
@@ -122,7 +122,7 @@ aws s3 sync --only-show-errors \
     --include '*.jpg' \
     --include '*.svg' \
     --include generated/headerFooter.json \
-    --include data/cms/*.json \
+    --include 'data/cms/*.json' \
     . "$DEST"
 
 # Sync content to s3
