8cc554cc5dd0bf9d41003d18a1b734d39bb11882
Darius Dzien <dariusz.dzien@adhocteam.us>
2022-07-19 15:05:30 -0500

    Clean up

--- config/cypress.config.js
+++ config/cypress.config.js
@@ -20,7 +20,7 @@ module.exports = defineConfig({
         config,
       );
     },
-    baseUrl: 'http://staging.va.gov',
+    baseUrl: 'http://localhost:3002',
     specPattern: './src/**/tests/**/*.cypress.spec.js?(x)',
     supportFile: 'src/platform/testing/e2e/cypress/support/index.js',
   },
