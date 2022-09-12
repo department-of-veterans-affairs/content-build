d3d70a5fcfa41993d146dffda2009d73a420aa32
Ryan Smith <ryan.smith@adhocteam.us>
2022-09-12 14:02:58 -0400

    Staging test

--- src/platform/site-wide/tests/sitemap/sitemap.a11y.spec.js
+++ src/platform/site-wide/tests/sitemap/sitemap.a11y.spec.js
@@ -3,9 +3,12 @@ const fetch = require('sync-fetch');
 const { normal } = require('../../../testing/e2e/timeouts');
 
 const step = Number(Cypress.env('STEP'));
-const data = fetch(
-  `http://localhost:${Cypress.env('CONTENT_BUILD_PORT')}/sitemap.xml`,
-).text();
+// TODO: Fetch staging version?
+const data = fetch(`http://staging.va.gov/sitemap.xml`).text();
+
+// const data = fetch(
+//   `http://localhost:${Cypress.env('CONTENT_BUILD_PORT')}/sitemap.xml`,
+// ).text();
 const urls = xml
   .parse(data)
   .urlset.url.map(url => url.loc)
@@ -25,10 +28,14 @@ describe(`Accessibility tests batch ${step} of 32`, () => {
   for (const url of splitURLs) {
     // eslint-disable-next-line no-loop-func
     it(`${url}`, () => {
-      const localURL = url.replace(
-        `https://www.va.gov`,
-        `http://localhost:${Cypress.env('CONTENT_BUILD_PORT')}`,
-      );
+      // TODO: When the site from the production build is started, the sitemap URLs are va.gov but it is running on localhost? If running against staging, no replacement needed?
+      const localURL = url;
+
+      // const localURL = url.replace(
+      //   `https://www.va.gov`,
+      //   `http://localhost:${Cypress.env('CONTENT_BUILD_PORT')}`,
+      // );
+
       cy.visit(localURL);
       cy.get('body').should('be.visible', { timeout: normal });
       // eslint-disable-next-line cypress/no-unnecessary-waiting
