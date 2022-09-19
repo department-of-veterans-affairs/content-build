643286c5d5155d893b04c38065ba41efe763d849
Bryan Paronto <bryan@kindsys.us>
2022-09-19 10:08:16 -0500

    updates to homepage prototype

--- src/site/stages/build/drupal/home.js
+++ src/site/stages/build/drupal/home.js
@@ -5,6 +5,19 @@ const yaml = require('js-yaml');
 const { createEntityUrlObj, createFileObj } = require('./page');
 const ENVIRONMENTS = require('../../../constants/environments');
 
+function divideHubRows(hubs) {
+  return hubs.map((hub, i) => {
+    // We want 3 cards per row.
+    if ((i + 1) % 3 === 0) {
+      hub = {
+        ...hub,
+        endRow: true,
+      };
+    }
+    return hub;
+  });
+}
+
 // Processes the data received from the home page query.
 function addHomeContent(contentData, files, metalsmith, buildOptions) {
   // We cannot limit menu items in Drupal, so we must do it here.
@@ -24,17 +37,8 @@ function addHomeContent(contentData, files, metalsmith, buildOptions) {
     } = contentData;
 
     // Liquid does not have a good modulo operator, so we let the template know when to end a row.
-    const hubs = homePageHubListQuery.itemsOfEntitySubqueueHomePageHubList.map(
-      (hub, i) => {
-        // We want 3 cards per row.
-        if ((i + 1) % 3 === 0) {
-          hub = {
-            ...hub,
-            endRow: true,
-          };
-        }
-        return hub;
-      },
+    const hubs = divideHubRows(
+      homePageHubListQuery.itemsOfEntitySubqueueHomePageHubList,
     );
 
     const fragmentsRoot = metalsmith.path(buildOptions.contentFragments);
@@ -57,19 +61,118 @@ function addHomeContent(contentData, files, metalsmith, buildOptions) {
       title: 'VA.gov Home',
     };
 
-    const homePreviewPath = '/homepage-test';
-    const homePreviewEntityObj = {
-      ...homeEntityObj,
-      path: homePreviewPath,
-      entityUrl: {
-        path: homePreviewPath,
-      },
-    };
-
     // Let Metalsmith know we're here.
     files[`./index.html`] = createFileObj(homeEntityObj, 'home.drupal.liquid');
 
+    /**
+     * The Below is meant to represent potentially dynamic content for the Home Page Prototype
+     * This page is only built on non-prod environments
+     * */
     if (buildOptions.buildtype !== ENVIRONMENTS.VAGOVPROD) {
+      const homePreviewPath = '/homepage-test';
+
+      const unsortedHubs =
+        homePageHubListQuery.itemsOfEntitySubqueueHomePageHubList;
+
+      // This assumes that benefit hubs with beresorted in the CMS later and for now is
+      // hardcoding their ordering to match the prototype mockup
+      let sortedHubs = [
+        unsortedHubs[9], // Health Care
+        unsortedHubs[10], // Disability
+        unsortedHubs[7], // Education & Training
+        //
+        unsortedHubs[3], // Careers and employment
+        unsortedHubs[5], // Pension
+        unsortedHubs[4], // Housing Assistance
+        //
+        unsortedHubs[6], // Life Insurance
+        unsortedHubs[2], // Burial and memorials
+        unsortedHubs[8], //
+
+        unsortedHubs[0], // Life Insurance
+        unsortedHubs[1], // Burial and memorials
+        unsortedHubs[9], // VA Dept Information -- Needs created,
+      ];
+
+      sortedHubs = divideHubRows(sortedHubs);
+
+      const homePreviewEntityObj = {
+        ...homeEntityObj,
+        hero: {
+          headline: 'The PACT Act and your VA benefits',
+          copy:
+            'Expands and extends eligibility for care and benefits for Veterans and Survivors related to toxic exposures.',
+          link: '#',
+          linkText: 'Learn about the PACT Act',
+        },
+        commonTasks: {
+          searchLinks: [
+            { link: '/find-a-location/', linkText: 'Find a VA facility' },
+            { link: '/find-forms/', linkText: 'Find a VA form' },
+            {
+              link: '/resouces/',
+              linkText: 'Find benedit resources and support',
+            },
+          ],
+          popularLinks: [
+            {
+              link: '/health-care/',
+              linkText: 'VA health care',
+            },
+            {
+              link: '/health-care/get-medical-records/',
+              linkText: 'VA medical records',
+            },
+            {
+              link: 'https://mentalhealth.va.gov/',
+              linkText: 'Mental health help',
+            },
+            {
+              link: '/health-care/get-reimbursed-for-travel-pay/',
+              linkText: 'Travel reimbursement for health care',
+            },
+            {
+              link: '/resources/the-pact-act-and-your-va-benefits/',
+              linkText: 'PACT Act information for Veterans with toxic exposure',
+            },
+            {
+              link: '/claim-or-appeal-status/',
+              linkText: 'Claim or appeal status',
+            },
+            {
+              link: '/disability/',
+              linkText: 'Disability compensation',
+            },
+            {
+              link: '/education/',
+              linkText: 'Education benefits',
+            },
+            {
+              link: '/va-payment-history/',
+              linkText: 'You VA payment history',
+            },
+            {
+              link: '/view-change-dependents',
+              linkText: 'Dependants on your disability benefits',
+            },
+          ],
+        },
+        blogPromo: {
+          title: 'Pathfinder: The front door for engaging with VA',
+          copy:
+            'Pathfinder is an entry point for those seeking to sell their product or service or innovate on solutions with VA.',
+          url: 'https://pathfinder.va.gov',
+          thumbnailAltText: 'Thumbnail alt text goes here',
+          thumbnail:
+            'https://s3-us-gov-west-1.amazonaws.com/content.www.va.gov/img/2022-08/Pathfinder%20VA.gov%20Banner%20%28552%20%C3%97%20360%20px%29_v1_0.png',
+        },
+        hubs: sortedHubs,
+        path: homePreviewPath,
+        entityUrl: {
+          path: homePreviewPath,
+        },
+      };
+
       files[`./homepage-test.html`] = createFileObj(
         homePreviewEntityObj,
         'home-preview.drupal.liquid',
