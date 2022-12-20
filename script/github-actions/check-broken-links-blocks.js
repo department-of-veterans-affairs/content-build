df62c69b9830dfc55450c2ee4ffc8ff5bef97023
Tim Cosgrove <timcosgrove@users.noreply.github.com>
2022-12-20 12:26:41 -0800

    Revert to older output setting syntax for broken link checking. (#1408)

--- script/github-actions/check-broken-links-blocks.js
+++ script/github-actions/check-broken-links-blocks.js
@@ -91,7 +91,7 @@ if (fs.existsSync(reportPath)) {
   console.log(
     `${brokenLinks.brokenLinksCount} broken links found. \n ${brokenLinks.summary}`,
   );
-  console.log(`SLACK_BLOCKS=${JSON.stringify(payload)} >> $GITHUB_OUTPUT`);
+  console.log(`::set-output name=SLACK_BLOCKS::${JSON.stringify(payload)}`);
 
   if (!IS_PROD_BRANCH && !contentOnlyBuild) {
     // Ignore the results of the broken link checker unless
@@ -106,12 +106,12 @@ if (fs.existsSync(reportPath)) {
    * Only emit this variable if ran against main branch or during Content Release.
    * Meets the following condition: blocks & attachments & IS_PROD_BRANCH
    */
-  console.log(`UPLOAD_AND_NOTIFY=1 >> $GITHUB_OUTPUT`);
+  console.log(`::set-output name=UPLOAD_AND_NOTIFY::1`);
 
   if (shouldFail) {
     throw new Error('Broken links found');
   }
 } else {
   console.log('No broken links found!');
-  console.log(`UPLOAD_AND_NOTIFY=0 >> $GITHUB_OUTPUT`);
+  console.log(`::set-output name=UPLOAD_AND_NOTIFY::0`);
 }
