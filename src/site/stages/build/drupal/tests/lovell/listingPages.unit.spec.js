28e6c1edb634ae622ffde5be5e884d097c74610a
Ryan Koch <ryan.koch.0213@gmail.com>
2022-12-29 12:29:25 -0600

    VACMS-11996: Improves Lovell listing-page tests - Adds tests for matching titles after merge - DRYs up code

--- src/site/stages/build/drupal/tests/lovell/listingPages.unit.spec.js
+++ src/site/stages/build/drupal/tests/lovell/listingPages.unit.spec.js
@@ -1,3 +1,5 @@
+/* eslint-disable @department-of-veterans-affairs/axe-check-required */
+/* eslint-disable camelcase */
 import { expect } from 'chai';
 import {
   processLovellPages,
@@ -16,6 +18,17 @@ import vaEvents from './fixtures/listing-pages/va/events.json';
 import vaNews from './fixtures/listing-pages/va/news.json';
 import lovellFederalHealthCareFacilitySidebarQuery from './fixtures/sidebar.json';
 
+const entityBundleFromListingVariant = listingVariant => {
+  let prefix = listingVariant;
+  if (listingVariant === 'stories') {
+    prefix = 'story';
+  } else if (listingVariant === 'events') {
+    prefix = 'event';
+  }
+
+  return `${prefix}_listing`;
+};
+
 const getMergedListing = (drupalData, lovellVariant, listingVariant) => {
   return drupalData.data.nodeQuery.entities
     .filter(page => {
@@ -28,11 +41,19 @@ const getMergedListing = (drupalData, lovellVariant, listingVariant) => {
 
       return false;
     })
-    .filter(page => page.entityBundle === `${listingVariant}_listing`)[0];
+    .filter(
+      page =>
+        page.entityBundle === entityBundleFromListingVariant(listingVariant),
+    )[0];
+};
+
+const stringArraysContainSameElements = (a, b) => {
+  return a.sort().join(',') === b.sort().join(',');
 };
 
 describe('processLovelPages (listing pages)', () => {
   let counts;
+  let titles;
   let drupalData;
 
   before(() => {
@@ -43,7 +64,7 @@ describe('processLovelPages (listing pages)', () => {
           past: federalEvents.pastEvents.entities.length,
           all: federalEvents.reverseFieldListingNode.entities.length,
         },
-        news: federalNews.reverseFieldListingNode.entities.length,
+        press_releases: federalNews.reverseFieldListingNode.entities.length,
       },
       tricare: {
         stories: tricareStories.reverseFieldListingNode.entities.length,
@@ -51,7 +72,7 @@ describe('processLovelPages (listing pages)', () => {
           past: tricareEvents.pastEvents.entities.length,
           all: tricareEvents.reverseFieldListingNode.entities.length,
         },
-        news: tricareNews.reverseFieldListingNode.entities.length,
+        press_releases: tricareNews.reverseFieldListingNode.entities.length,
       },
       va: {
         stories: vaStories.reverseFieldListingNode.entities.length,
@@ -59,7 +80,52 @@ describe('processLovelPages (listing pages)', () => {
           past: vaEvents.pastEvents.entities.length,
           all: vaEvents.reverseFieldListingNode.entities.length,
         },
-        news: vaNews.reverseFieldListingNode.entities.length,
+        press_releases: vaNews.reverseFieldListingNode.entities.length,
+      },
+    };
+
+    titles = {
+      federal: {
+        stories: federalStories.reverseFieldListingNode.entities.map(
+          entity => entity.title,
+        ),
+        events: {
+          past: federalEvents.pastEvents.entities.map(entity => entity.title),
+          all: federalEvents.reverseFieldListingNode.entities.map(
+            entity => entity.title,
+          ),
+        },
+        press_releases: federalNews.reverseFieldListingNode.entities.map(
+          entity => entity.title,
+        ),
+      },
+      tricare: {
+        stories: tricareStories.reverseFieldListingNode.entities.map(
+          entity => entity.title,
+        ),
+        events: {
+          past: tricareEvents.pastEvents.entities.map(entity => entity.title),
+          all: tricareEvents.reverseFieldListingNode.entities.map(
+            entity => entity.title,
+          ),
+        },
+        press_releases: tricareNews.reverseFieldListingNode.entities.map(
+          entity => entity.title,
+        ),
+      },
+      va: {
+        stories: vaStories.reverseFieldListingNode.entities.map(
+          entity => entity.title,
+        ),
+        events: {
+          past: vaEvents.pastEvents.entities.map(entity => entity.title),
+          all: vaEvents.reverseFieldListingNode.entities.map(
+            entity => entity.title,
+          ),
+        },
+        press_releases: vaNews.reverseFieldListingNode.entities.map(
+          entity => entity.title,
+        ),
       },
     };
 
@@ -85,70 +151,93 @@ describe('processLovelPages (listing pages)', () => {
     processLovellPages(drupalData);
   });
 
-  describe('stories listing pages', () => {
-    it('correctly merges Federal and Tricare stories for Tricare listing page', () => {
-      const mergedStories = getMergedListing(drupalData, 'tricare', 'story');
-      const mergedStoryCount =
-        mergedStories.reverseFieldListingNode.entities.length;
-      expect(mergedStoryCount).to.equal(
-        counts.federal.stories + counts.tricare.stories,
-      );
-    });
-    it('correctly merges Federal and VA stories for VA listing page', () => {
-      const mergedStories = getMergedListing(drupalData, 'va', 'story');
-      const mergedStoryCount =
-        mergedStories.reverseFieldListingNode.entities.length;
-      expect(mergedStoryCount).to.equal(
-        counts.federal.stories + counts.va.stories,
-      );
-    });
-  });
+  const testMergedListingCounts = (
+    mergedListing,
+    lovellVariant,
+    listingVariant,
+  ) => {
+    const mergedListingCount =
+      mergedListing.reverseFieldListingNode.entities.length;
+    const expectedMergedListingCount =
+      listingVariant === 'events'
+        ? counts.federal.events.all + counts[lovellVariant].events.all
+        : counts.federal[listingVariant] +
+          counts[lovellVariant][listingVariant];
+    expect(mergedListingCount).to.equal(expectedMergedListingCount);
+  };
 
-  describe('events listing pages', () => {
-    it('correctly merges Federal and Tricare events for Tricare listing page', () => {
-      const mergedEvents = getMergedListing(drupalData, 'tricare', 'event');
-      const mergedAllEventsCount =
-        mergedEvents.reverseFieldListingNode.entities.length;
-      const mergedPastEventsCount = mergedEvents.pastEvents.entities.length;
-      expect(mergedAllEventsCount).to.equal(
-        counts.federal.events.all + counts.tricare.events.all,
-      );
-      expect(mergedPastEventsCount).to.equal(
-        counts.federal.events.past + counts.tricare.events.past,
-      );
-    });
-    it('correctly merges Federal and VA events for VA listing page', () => {
-      const mergedEvents = getMergedListing(drupalData, 'va', 'event');
-      const mergedAllEventsCount =
-        mergedEvents.reverseFieldListingNode.entities.length;
-      const mergedPastEventsCount = mergedEvents.pastEvents.entities.length;
-      expect(mergedAllEventsCount).to.equal(
-        counts.federal.events.all + counts.va.events.all,
-      );
-      expect(mergedPastEventsCount).to.equal(
-        counts.federal.events.past + counts.va.events.past,
-      );
-    });
-  });
+  const testMergedListingTitles = (
+    mergedListing,
+    lovellVariant,
+    listingVariant,
+  ) => {
+    const mergedListingTitles = mergedListing.reverseFieldListingNode.entities.map(
+      entity => entity.title,
+    );
+    const expectedMergedListingTitles =
+      listingVariant === 'events'
+        ? [
+            ...titles.federal[listingVariant].all,
+            ...titles[lovellVariant][listingVariant].all,
+          ]
+        : [
+            ...titles.federal[listingVariant],
+            ...titles[lovellVariant][listingVariant],
+          ];
+    expect(
+      stringArraysContainSameElements(
+        mergedListingTitles,
+        expectedMergedListingTitles,
+      ),
+    ).to.be.true;
+  };
 
-  describe('press-release listing pages', () => {
-    it('correctly merges Federal and Tricare press releases for Tricare listing page', () => {
-      const mergedNews = getMergedListing(
-        drupalData,
-        'tricare',
-        'press_releases',
-      );
-      const mergedNewsCount =
-        mergedNews.reverseFieldListingNode.entities.length;
-      expect(mergedNewsCount).to.equal(
-        counts.federal.news + counts.tricare.news,
-      );
-    });
-    it('correctly merges Federal and VA press releases for VA listing page', () => {
-      const mergedNews = getMergedListing(drupalData, 'va', 'press_releases');
-      const mergedNewsCount =
-        mergedNews.reverseFieldListingNode.entities.length;
-      expect(mergedNewsCount).to.equal(counts.federal.news + counts.va.news);
+  const testMergedPastEvents = (mergedEvents, lovellVariant) => {
+    const mergedPastEventsCount = mergedEvents.pastEvents.entities.length;
+    expect(mergedPastEventsCount).to.equal(
+      counts.federal.events.past + counts[lovellVariant].events.past,
+    );
+
+    const mergedPastEventTitles = mergedEvents.pastEvents.entities.map(
+      entity => entity.title,
+    );
+    const expectedPastEventTitles = [
+      ...titles.federal.events.past,
+      ...titles[lovellVariant].events.past,
+    ];
+    expect(
+      stringArraysContainSameElements(
+        mergedPastEventTitles,
+        expectedPastEventTitles,
+      ),
+    ).to.be.true;
+  };
+
+  // Create tests for each of six (6) listing pages
+  // - VA Stories
+  // - VA Events
+  // - VA Press Releases
+  // - Tricare Stories
+  // - Tricare Events
+  // - Tricare Press Releases
+  ['stories', 'events', 'press_releases'].forEach(listingVariant => {
+    describe(`${listingVariant} listing pages`, () => {
+      ['va', 'tricare'].forEach(lovellVariant => {
+        it(`correctly merges federal and ${lovellVariant} ${listingVariant} for ${lovellVariant} listing page`, () => {
+          const mergedListing = getMergedListing(
+            drupalData,
+            lovellVariant,
+            listingVariant,
+          );
+
+          testMergedListingCounts(mergedListing, lovellVariant, listingVariant);
+          testMergedListingTitles(mergedListing, lovellVariant, listingVariant);
+
+          if (listingVariant === 'events') {
+            testMergedPastEvents(mergedListing, lovellVariant);
+          }
+        });
+      });
     });
   });
 });
