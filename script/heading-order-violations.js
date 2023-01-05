e6db7164a703cbce2aa2dd7b92dcf56a6da8a6b0
Nathan Douglas <github@darkdell.net>
2023-01-05 10:13:48 -0500

    Refine script.

--- script/heading-order-violations.js
+++ script/heading-order-violations.js
@@ -6,37 +6,28 @@ const glob = require('glob');
 const optionDefinitions = [{ name: 'dir', alias: 'd' }];
 const options = commandLineArgs(optionDefinitions);
 
-function handleError(error) {
-  console.log(error);
+if (options.help || !options.dir) {
+  process.exit(0);
 }
 
-function main() {
-  if (options.help || !options.dir) {
-    process.exit(0);
+const htmlFiles = glob.sync(`${options.dir}/**/*.html`);
+
+htmlFiles.forEach((filename, index, array) => {
+  console.error(`Processing file ${index} of ${array.length}`);
+  try {
+    const data = fs.readFileSync(filename, 'utf8');
+    const badHeadings = data
+      .match(
+        new RegExp(
+          /(<h1((?!<h[12]).)*<h[3-6]|<h2((?!<h[1-3]).)*<h[4-6]|<h3((?!<h[1-4]).)*<h[56]|<h4((?!<h[1-5]).)*<h6)/,
+          'gsm',
+        ),
+      )
+      ?.join();
+    if (badHeadings) {
+      console.log(filename);
+    }
+  } catch (err) {
+    console.error(err);
   }
-
-  const htmlFiles = glob.sync(`${options.dir}/**/*.html`);
-
-  htmlFiles.forEach(fname => {
-    /* eslint-disable-next-line consistent-return */
-    fs.readFile(fname, 'utf8', (err, data) => {
-      if (err) {
-        return handleError(err);
-      }
-
-      const h1ToNextHeading = data
-        .match(
-          new RegExp(
-            /(<h1((?!<h[12]).)*<h[3-6]|<h2((?!<h[1-3]).)*<h[4-6]|<h3((?!<h[1-4]).)*<h[56]|<h4((?!<h[1-5]).)*<h6)/,
-            'gsm',
-          ),
-        )
-        ?.join();
-
-      if (!!h1ToNextHeading && !h1ToNextHeading?.includes('<h2')) {
-        console.log(fname.slice(1));
-      }
-    });
-  });
-}
-main();
+});
