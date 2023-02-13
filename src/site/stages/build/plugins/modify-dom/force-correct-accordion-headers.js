40120dfa32d995dd66368fea5f84b543bc1ac704
Bryan Paronto <bryan@kindsys.us>
2023-02-13 11:02:45 -0600

    12342 refactoring and cleaning up

--- src/site/stages/build/plugins/modify-dom/force-correct-accordion-headers.js
+++ src/site/stages/build/plugins/modify-dom/force-correct-accordion-headers.js
@@ -8,6 +8,23 @@
 
 const headerHierarchy = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
 
+function correctHeadingLevel(index, accordionNode, headerLevelIndex, dom) {
+  const accordionHeaders = dom(accordionNode).find('h2, h3, h4, h5, h6');
+  accordionHeaders.each((_, accordionHeader) => {
+    const accordionHeaderTagName = accordionHeader.tagName;
+    const currentIndex = headerHierarchy.indexOf(accordionHeaderTagName);
+
+    if (
+      (currentIndex - headerLevelIndex >= 2 ||
+        currentIndex - headerLevelIndex <= 0) &&
+      !accordionHeader.changed
+    ) {
+      accordionHeader.tagName = headerHierarchy[headerLevelIndex + 1];
+      accordionHeader.changed = true;
+    }
+  });
+}
+
 module.exports = {
   initialize() {
     console.time('Force Correct Accordion Level');
@@ -17,45 +34,60 @@ module.exports = {
   },
   modifyFile(fileName, file) {
     let didModifyHeaders = false;
-    if (fileName.endsWith('html')) {
-      const { dom } = file;
-      const headerNodes = dom(
-        '[data-template="paragraphs/wysiwyg"] div h2, [data-template="paragraphs/wysiwyg"] div h3, [data-template="paragraphs/wysiwyg"] div h4, [data-template="paragraphs/wysiwyg"] div h5, [data-template="paragraphs/wysiwyg"] div h6',
-      );
-      if (headerNodes.length > 0) {
-        headerNodes.each((index, el) => {
-          const headerLevel = el.tagName;
-          const headerLevelIndex = headerHierarchy.indexOf(headerLevel);
-          const accordionNodes = dom(el)
-            .parent()
-            .parent()
-            .siblings('[data-template="paragraphs/collapsible_panel"]')
-            .children('va-accordion')
-            .children('va-accordion-item');
-          if (accordionNodes.length > 0) {
-            accordionNodes.each((i, accordionNode) => {
-              const accordionHeaders = dom(accordionNode).find(
-                'h2, h3, h4, h5, h6',
-              );
-              accordionHeaders.each((_, accordionHeader) => {
-                const accordionHeaderTagName = accordionHeader.tagName;
-                const currentIndex = headerHierarchy.indexOf(
-                  accordionHeaderTagName,
-                );
-                if (currentIndex - headerLevelIndex >= 2) {
-                  accordionHeader.tagName =
-                    headerHierarchy[headerLevelIndex + 1];
-                  console.log(
-                    `changed accordion header ${fileName}-${index}-${i}-${_} from ${accordionHeaderTagName} to ${accordionHeader.tagName}`,
-                  );
-                  didModifyHeaders = true;
-                }
-              });
-            });
-          }
-        });
-      }
+
+    if (!fileName.endsWith('html')) {
+      return;
     }
+
+    const { dom } = file;
+
+    // Bail early if page doesnt have an Accordion
+    if (dom("[data-template='paragraphs/collapsible_panel']").length < 1) {
+      return;
+    }
+
+    const headerNodes = dom(
+      '[data-template="paragraphs/wysiwyg"] div h2, [data-template="paragraphs/wysiwyg"] div h3, [data-template="paragraphs/wysiwyg"] div h4, [data-template="paragraphs/wysiwyg"] div h5, [data-template="paragraphs/wysiwyg"] div h6',
+    );
+
+    if (headerNodes.length > 0) {
+      headerNodes.each((index, el) => {
+        const headerLevel = el.tagName;
+        const headerLevelIndex = headerHierarchy.indexOf(headerLevel);
+
+        const accordionNodes = dom(el)
+          .parent()
+          .parent()
+          .siblings('[data-template="paragraphs/collapsible_panel"]')
+          .children('va-accordion')
+          .children('va-accordion-item');
+
+        if (accordionNodes.length > 0) {
+          accordionNodes.each((idx, element) =>
+            correctHeadingLevel(idx, element, headerLevelIndex, dom),
+          );
+          didModifyHeaders = true;
+        }
+      });
+    } else {
+      dom('h1').each((index, el) => {
+        const headerLevel = el.tagName;
+        const headerLevelIndex = headerHierarchy.indexOf(headerLevel);
+
+        const accordionNodes = dom(el)
+          .siblings('[data-template="paragraphs/collapsible_panel"]')
+          .children('va-accordion')
+          .children('va-accordion-item');
+
+        if (accordionNodes.length > 0) {
+          accordionNodes.each((idx, element) =>
+            correctHeadingLevel(idx, element, headerLevelIndex, dom),
+          );
+          didModifyHeaders = true;
+        }
+      });
+    }
+
     if (didModifyHeaders) {
       file.modified = true;
     }
