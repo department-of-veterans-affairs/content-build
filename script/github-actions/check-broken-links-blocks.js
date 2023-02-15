0a890a1044d96af0759e0817ff7504618ce07d94
Tim Cosgrove <tim.cosgrove@agile6.com>
2023-02-15 15:30:23 -0800

    Updates to setting output for broken link action.

--- script/github-actions/check-broken-links-blocks.js
+++ script/github-actions/check-broken-links-blocks.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 const fs = require('fs');
+const core = require('@actions/core');
 
 const args = process.argv.slice(2);
 const envName = args[0];
@@ -91,7 +92,7 @@ if (fs.existsSync(reportPath)) {
   console.log(
     `${brokenLinks.brokenLinksCount} broken links found. \n ${brokenLinks.summary}`,
   );
-  console.log(`::set-output name=SLACK_BLOCKS::${JSON.stringify(payload)}`);
+  core.setOutput('SLACK_BLOCKS', `${JSON.stringify(payload)}`);
 
   if (!IS_PROD_BRANCH && !contentOnlyBuild) {
     // Ignore the results of the broken link checker unless
@@ -106,12 +107,11 @@ if (fs.existsSync(reportPath)) {
    * Only emit this variable if ran against main branch or during Content Release.
    * Meets the following condition: blocks & attachments & IS_PROD_BRANCH
    */
-  console.log(`::set-output name=UPLOAD_AND_NOTIFY::1`);
-
+  core.setOutput('UPLOAD_AND_NOTIFY', '1');
   if (shouldFail) {
     throw new Error('Broken links found');
   }
 } else {
   console.log('No broken links found!');
-  console.log(`::set-output name=UPLOAD_AND_NOTIFY::0`);
+  core.setOutput('UPLOAD_AND_NOTIFY', '0');
 }
