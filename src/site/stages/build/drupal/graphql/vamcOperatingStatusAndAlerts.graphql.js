a4235217e0ba99a3c548e7453330eb08e3131df2
Ryan Koch <ryan.koch.0213@gmail.com>
2023-03-02 10:37:02 -0600

    Revert "[VA-12570] Update COVID status field with facility-specific supplemental info (#1479)"
    This reverts commit 13cf39704c2e178430ed6cbbcd46b9de730c031b.

--- src/site/stages/build/drupal/graphql/vamcOperatingStatusAndAlerts.graphql.js
+++ src/site/stages/build/drupal/graphql/vamcOperatingStatusAndAlerts.graphql.js
@@ -29,11 +29,6 @@ const vamcOperatingStatusAndAlerts = `
                     }
                   }
                 }
-                fieldSupplementalStatusMoreI {
-                  value
-                  format
-                  processed
-                }
               }
             }
           }
