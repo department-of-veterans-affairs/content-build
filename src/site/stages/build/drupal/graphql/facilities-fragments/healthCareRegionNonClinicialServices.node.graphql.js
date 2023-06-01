cab62fa0ce74b3102a8672d9b0c0d4f8ebf38600
Ryan Koch <ryan.koch.0213@gmail.com>
2023-06-01 08:48:31 -0500

    VACMS-13199: Adds node-status filter to subqueries for non-clinical services.

--- src/site/stages/build/drupal/graphql/facilities-fragments/healthCareRegionNonClinicialServices.node.graphql.js
+++ src/site/stages/build/drupal/graphql/facilities-fragments/healthCareRegionNonClinicialServices.node.graphql.js
@@ -15,7 +15,15 @@ module.exports = `
   entity {
     entityId
     entityLabel
-    reverseFieldRegionPageNode(limit: 50, filter: {conditions: [{field: "type", value: ["health_care_local_facility"]}]}) {
+    reverseFieldRegionPageNode(
+      limit: 50,
+      filter: {
+        conditions: [
+          { field: "type", value: ["health_care_local_facility"] },
+          { field: "status", value: ["1"], enabled: $onlyPublishedContent }
+        ]
+      }
+    ) {
       entities {
         ... on NodeHealthCareLocalFacility {
           entityLabel
@@ -31,9 +39,8 @@ module.exports = `
           ${FIELD_ADDRESS}
           reverseFieldFacilityLocationNode(limit: 50, filter: {
             conditions: [
-              {
-                field: "type", value: ["vha_facility_nonclinical_service"]
-              }
+              { field: "type", value: ["vha_facility_nonclinical_service"] },
+              { field: "status", value: ["1"], enabled: $onlyPublishedContent }
             ]
           }) {
             entities {
