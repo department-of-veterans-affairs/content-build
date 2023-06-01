67f61ec73758ef51e8b3b979f98ef445dd386bd4
Tim Cosgrove <timcosgrove@users.noreply.github.com>
2023-06-01 15:00:18 -0700

    Disable the homepage cypress test.

--- src/site/tests/home/00-required.cypress.spec.js
+++ src/site/tests/home/00-required.cypress.spec.js
@@ -1,33 +1,33 @@
-const path = require('path');
+// const path = require('path');
 
-Cypress.Commands.add('verifyGoogleAnalytics', () => {
-  const filePath = path.join(
-    'content-build',
-    __dirname,
-    '..',
-    '..',
-    'assets/js/google-analytics/',
-    `${Cypress.env('buildtype') || 'vagovdev'}.js`,
-  );
-  cy.readFile(filePath).then(str => {
-    cy.get('[data-e2e="analytics-script"]')
-      .invoke('html')
-      .then(value => {
-        expect(value.replace(/\s/g, '')).to.contain(str.replace(/\s/g, ''));
-      });
-  });
-});
+// Cypress.Commands.add('verifyGoogleAnalytics', () => {
+//   const filePath = path.join(
+//     'content-build',
+//     __dirname,
+//     '..',
+//     '..',
+//     'assets/js/google-analytics/',
+//     `${Cypress.env('buildtype') || 'vagovdev'}.js`,
+//   );
+//   cy.readFile(filePath).then(str => {
+//     cy.get('[data-e2e="analytics-script"]')
+//       .invoke('html')
+//       .then(value => {
+//         expect(value.replace(/\s/g, '')).to.contain(str.replace(/\s/g, ''));
+//       });
+//   });
+// });
 
-Cypress.Commands.add('verifyElementCount', (selector, expectedLength) => {
-  cy.get(selector).should('have.length', expectedLength);
-});
+// Cypress.Commands.add('verifyElementCount', (selector, expectedLength) => {
+//   cy.get(selector).should('have.length', expectedLength);
+// });
 
-describe('Homepage Test', () => {
-  it('has the right elements and analytics', () => {
-    cy.visit('/');
-    cy.verifyElementCount('[data-e2e="bucket"]', 4);
-    cy.verifyElementCount('[data-e2e="hub"]', 11);
-    cy.verifyElementCount('[data-e2e="news"]', 3);
-    cy.verifyGoogleAnalytics();
-  });
-});
+// describe('Homepage Test', () => {
+//   it('has the right elements and analytics', () => {
+//     cy.visit('/');
+//     cy.verifyElementCount('[data-e2e="bucket"]', 4);
+//     cy.verifyElementCount('[data-e2e="hub"]', 11);
+//     cy.verifyElementCount('[data-e2e="news"]', 3);
+//     cy.verifyGoogleAnalytics();
+//   });
+// });
