02b1a2f021526500223d8011fefb8f41953ff28e
Holden Hinkle <holden@holdenhinkle.com>
2023-06-16 09:48:28 -0400

    testing API_URL - appending 'bumfuzzle' to 'api'

--- src/site/constants/environments-configs.js
+++ src/site/constants/environments-configs.js
@@ -27,19 +27,19 @@ module.exports = {
   [ENVIRONMENTS.VAGOVPROD]: {
     BUILDTYPE: ENVIRONMENTS.VAGOVPROD,
     BASE_URL: 'https://www.va.gov',
-    API_URL: 'https://api.va.gov',
+    API_URL: 'https://api-bumfuzzle.va.gov',
   },
 
   [ENVIRONMENTS.VAGOVSTAGING]: {
     BUILDTYPE: ENVIRONMENTS.VAGOVSTAGING,
     BASE_URL: 'https://staging.va.gov',
-    API_URL: 'https://staging-api.va.gov',
+    API_URL: 'https://staging-api-bumfuzzle.va.gov',
   },
 
   [ENVIRONMENTS.VAGOVDEV]: {
     BUILDTYPE: ENVIRONMENTS.VAGOVDEV,
     BASE_URL: 'https://dev.va.gov',
-    API_URL: 'https://dev-api.va.gov',
+    API_URL: 'https://dev-api-bumfuzzle.va.gov',
   },
 
   /* eslint-disable no-restricted-globals */
@@ -59,7 +59,9 @@ module.exports = {
           '*.preview.va.gov',
           '*.vfs.va.gov',
         ])
-      ? `http://${location.hostname.split('.')[0]}-api.${location.hostname
+      ? `http://${
+          location.hostname.split('.')[0]
+        }-api-bumfuzzle.${location.hostname
           .split('.')
           .slice(1)
           .join('.')}`
