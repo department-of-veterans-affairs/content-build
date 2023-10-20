c911d2b363d81920f470a59713ba212dae157b65
Eli Selkin <eli.selkin@agile6.com>
2023-10-19 21:44:47 -0700

    will files be present in build

--- src/site/stages/build/drupal/static-data-files/vaPoliceData/index.js
+++ src/site/stages/build/drupal/static-data-files/vaPoliceData/index.js
@@ -15,6 +15,7 @@ const query = (inputs, outputFilenames) => async () => {
   if (inputs.length !== outputFilenames.length) {
     throw new Error('inputs and output_filenames must be the same length');
   }
+  const outputData = [];
   for await (const io of zip(inputs, outputFilenames)) {
     if (typeof io[0] !== 'string') {
       throw new Error('inputs must be an array of strings');
@@ -24,20 +25,25 @@ const query = (inputs, outputFilenames) => async () => {
     }
     if (/https?|file:\/\//.test(io[0])) {
       const { data } = await curly.get(io[0]);
-      fs.writeFileSync(path.join(__dirname, io[1]), data);
+      outputData.push(data);
     }
   }
-  return outputFilenames;
+  return { outputFilenames, outputData };
 };
 
 /* eslint-disable camelcase */
 const postProcess = async queryResult => {
-  log({ POLICE: JSON.stringify(queryResult) });
+  log({ POLICE: queryResult });
   const processedJSON = {};
-  for await (const csvFilePath of queryResult) {
-    const data = await csv().fromFile(csvFilePath);
-    log(data);
-  }
+  const [contact, events] = queryResult.outputData;
+  const contactFile = path.join(__dirname, 'pre-contact-police.csv');
+  const eventsFile = path.join(__dirname, 'pre-events-police.csv');
+  fs.writeFileSync(contactFile, contact);
+  fs.writeFileSync(eventsFile, events);
+  const jsonContact = await csv().fromFile(contactFile);
+  const jsonEvents = await csv().fromFile(eventsFile);
+  log(jsonContact);
+  log(jsonEvents);
   return processedJSON;
 };
 
