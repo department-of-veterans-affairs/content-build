c4a64804de423db93cb374d1e57a07a94256bd7a
Eli Selkin <eli.selkin@agile6.com>
2023-12-06 14:23:14 -0800

    staging -> prod

--- src/site/stages/build/drupal/static-data-files/vamcSystem/index.js
+++ src/site/stages/build/drupal/static-data-files/vamcSystem/index.js
@@ -2,7 +2,7 @@ const { postProcessVamcSystems } = require('./postProcessVamcSystem');
 
 // URLs to fetch (even if they are local files)
 const query = [
-  'http://staging.cms.va.gov/admin/content/exports/vamc-facilities-csv',
+  'http://prod.cms.va.gov/admin/content/exports/vamc-facilities-csv',
 ];
 
 const postProcess = postProcessVamcSystems;
