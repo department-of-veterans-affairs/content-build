c46240866180026e28cc028d0f837540c15a4fb8
Eli Selkin <eli.selkin@agile6.com>
2023-12-21 16:35:51 -0800

    working VA Police Cypress tests

--- src/site/tests/cypress/va-police.cypress.spec.js
+++ src/site/tests/cypress/va-police.cypress.spec.js
@@ -1,4 +1,4 @@
-describe('VAMC System - VA Police Sidebar', () => {
+describe('VAMC System - VA Police', () => {
   it('Has VA Police in the sidebar', () => {
     cy.visit('/maine-health-care');
     cy.injectAxeThenAxeCheck();
@@ -6,6 +6,19 @@ describe('VAMC System - VA Police Sidebar', () => {
       .find('.va-sidenav-level-1')
       .should('be.visible')
       .should('have.length', 3);
-    cy.findByText('VA police').should('be.visible');
+    cy.findByText('VA police')
+      .should('be.visible')
+      .click();
+    cy.location('pathname', { timeout: 6000 }).should('include', '/va-police');
+  });
+  it('Has VA Police page', () => {
+    cy.visit('/maine-health-care/va-police');
+    cy.injectAxeThenAxeCheck();
+    cy.contains('h1', 'VA police');
+    cy.contains('.va-introtext', 'VA police officers');
+    cy.get('va-on-this-page')
+      .shadow()
+      .find('a')
+      .should('have.length', 3);
   });
 });
