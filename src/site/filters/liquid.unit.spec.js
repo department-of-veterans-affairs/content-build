f8ffbe21c76c0c687fe558ec7985aa8a0e37e477
Eli Selkin <eli.j.selkin@gmail.com>
2024-01-03 08:08:07 -0800

    Testing VA police with liquid and fixtures (#1832)
    * working query and beginning of page - not clickable sidenav for VA Police
    
    * shaping up with FAQ and feature
    
    * leave off date, h2 as h3 visually on police report
    
    * Lovell working
    
    * potential for future Lovell switching
    
    * something not working
    
    * contact
    
    * remove back to top for MMVP
    
    * va-telephone is hiding sentence after
    
    * working va-telephone
    
    * correct order
    
    * testing police fixtures to build HTML from liquid
    
    * fix spacings
    
    * cypress
    
    * working VA Police Cypress tests
    
    * mocks and testing
    
    * more tests for liquid filters

--- src/site/filters/liquid.unit.spec.js
+++ src/site/filters/liquid.unit.spec.js
@@ -19,6 +19,8 @@ import prodSurveys from './medalliaProdSurveys.json';
 import vbaDataCantFind from '../layouts/tests/vba/template/fixtures/vba_facility_data_cant_find_benefits.json';
 import vbaDataBenefitHotline from '../layouts/tests/vba/template/fixtures/vba_facility_data_benefits_hotline.json';
 import vbaDataUpdates from '../layouts/tests/vba/template/fixtures/vba_facility_data_updates.json';
+import phoneMockData from '../layouts/tests/vamc/fixtures/phoneMockData.json';
+import simpleWysiwygMockData from '../layouts/tests/vamc/fixtures/simpleWysiwygMockData.json';
 
 registerFilters();
 
@@ -1660,6 +1662,37 @@ describe('processCentralizedUpdatesVBA', () => {
     );
     expect(Object.keys(data.links).length).to.be.equal(
       vbaDataUpdates.fetched.fieldLinks.length,
+    );
+  });
+});
+
+describe('processFieldPhoneNumbersParagraph', () => {
+  it('returns null if null is passed', () => {
+    expect(liquid.filters.processFieldPhoneNumbersParagraph(null)).to.be.null;
+  });
+  it('returns simple object of phone numbers', () => {
+    const data = liquid.filters.processFieldPhoneNumbersParagraph(
+      phoneMockData,
+    );
+    expect(data.contact).to.be.equal(phoneMockData[0].entity.fieldPhoneNumber);
+    expect(data.extension).to.be.equal(
+      phoneMockData[0].entity.fieldPhoneExtension,
+    );
+  });
+});
+describe('processWysiwygSimple', () => {
+  it('returns null if null is passed', () => {
+    expect(liquid.filters.processWysiwygSimple(null)).to.be.null;
+  });
+  it('returns null if wysiwyg is empty list', () => {
+    expect(
+      liquid.filters.processWysiwygSimple({ fetched: { fieldWysiwyg: [] } }),
+    ).to.be.null;
+  });
+  it('returns simple object of wysiwyg', () => {
+    const data = liquid.filters.processWysiwygSimple(simpleWysiwygMockData);
+    expect(data).to.be.equal(
+      simpleWysiwygMockData.fetched.fieldWysiwyg[0].value,
     );
   });
 });
