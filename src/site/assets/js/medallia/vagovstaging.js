350ac6576104024ae67d6fb652c98c8fbb025732
aubreyarcangel <aarcangel@gmail.com>
2024-02-28 15:22:39 -0500

    cleaned up medallia header code

--- src/site/assets/js/medallia/vagovstaging.js
+++ src/site/assets/js/medallia/vagovstaging.js
@@ -1,5 +1,3 @@
-const { SURVEY_NUMBERS, medalliaSurveys } = require('../../../filters/medalliaSurveysConfig')
-
 (function () {
   if (window.KAMPYLE_ONSITE_SDK) {
       onsiteLoaded();
@@ -16,74 +14,47 @@ function onsiteLoaded() {
   }
 }
 
-// const medalliaSurveys = {
-//   "urls":  {
-//     "/search": {
-//       "production": 21,
-//       "staging": 20
-//     },
-//     "/contact-us/virtual-agent": {
-//       "production": 25,
-//       "staging": 26
-//     },
-//     "/school-administrators": {
-//       "production": 17,
-//       "staging": 37
-//     }
-//   },
+const SURVEY_NUMBERS = {
+  DEFAULT_STAGING_SURVEY: 11,
+  DEFAULT_PROD_SURVEY: 17,
+  SEARCH_PROD: 21,
+  SEARCH_STAGING: 20,
+  CONTACT_US_VIRTUAL_AGENT_PROD: 25,
+  CONTACT_US_VIRTUAL_AGENT_STAGING: 26,
+  SCHOOL_ADMINISTRATORS_PROD: 17,
+  SCHOOL_ADMINISTRATORS_STAGING: 37,
+  HEALTH_CARE_PROD: 17,
+  HEALTH_CARE_STAGING: 41,
+  VISIT_SUMMARY_PROD: 17,
+  VISIT_SUMMARY_STAGING: 41,
+};
 
-//   "urlsWithSubPaths": {
-//     "/health-care": {
-//       "production": 17,
-//       "staging": 41
-//     },
-//     "/my-health/medical-records/summaries-and-notes/visit-summary": {
-//       "production": 17,
-//       "staging": 41
-//     }   
-//   }
-// }
-// const SURVEY_NUMBERS = {
-//   DEFAULT_STAGING_SURVEY: 11,
-//   DEFAULT_PROD_SURVEY: 17,
-//   SEARCH_PROD: 21,
-//   SEARCH_STAGING: 20,
-//   CONTACT_US_VIRTUAL_AGENT_PROD: 25,
-//   CONTACT_US_VIRTUAL_AGENT_STAGING: 26,
-//   SCHOOL_ADMINISTRATORS_PROD: 17,
-//   SCHOOL_ADMINISTRATORS_STAGING: 37,
-//   HEALTH_CARE_PROD: 17,
-//   HEALTH_CARE_STAGING: 41,
-//   VISIT_SUMMARY_PROD: 17,
-//   VISIT_SUMMARY_STAGING: 41,
-// };
-
-// const medalliaSurveys = {
-//   urls: {
-//     '/search': {
-//       production: SURVEY_NUMBERS.SEARCH_PROD,
-//       staging: SURVEY_NUMBERS.SEARCH_STAGING,
-//     },
-//     '/contact-us/virtual-agent': {
-//       production: SURVEY_NUMBERS.CONTACT_US_VIRTUAL_AGENT_PROD,
-//       staging: SURVEY_NUMBERS.CONTACT_US_VIRTUAL_AGENT_STAGING,
-//     },
-//     '/school-administrators': {
-//       production: SURVEY_NUMBERS.SCHOOL_ADMINISTRATORS_PROD,
-//       staging: SURVEY_NUMBERS.SCHOOL_ADMINISTRATORS_STAGING,
-//     },
-//   },
-//   urlsWithSubPaths: {
-//     '/health-care': {
-//       production: SURVEY_NUMBERS.HEALTH_CARE_PROD,
-//       staging: SURVEY_NUMBERS.HEALTH_CARE_STAGING,
-//     },
-//     '/my-health/medical-records/summaries-and-notes/visit-summary': {
-//       production: SURVEY_NUMBERS.VISIT_SUMMARY_PROD,
-//       staging: SURVEY_NUMBERS.VISIT_SUMMARY_STAGING,
-//     },
-//   },
-// };
+const medalliaSurveys = {
+  urls: {
+    '/search': {
+      production: SURVEY_NUMBERS.SEARCH_PROD,
+      staging: SURVEY_NUMBERS.SEARCH_STAGING,
+    },
+    '/contact-us/virtual-agent': {
+      production: SURVEY_NUMBERS.CONTACT_US_VIRTUAL_AGENT_PROD,
+      staging: SURVEY_NUMBERS.CONTACT_US_VIRTUAL_AGENT_STAGING,
+    },
+    '/school-administrators': {
+      production: SURVEY_NUMBERS.SCHOOL_ADMINISTRATORS_PROD,
+      staging: SURVEY_NUMBERS.SCHOOL_ADMINISTRATORS_STAGING,
+    },
+  },
+  urlsWithSubPaths: {
+    '/health-care': {
+      production: SURVEY_NUMBERS.HEALTH_CARE_PROD,
+      staging: SURVEY_NUMBERS.HEALTH_CARE_STAGING,
+    },
+    '/my-health/medical-records/summaries-and-notes/visit-summary': {
+      production: SURVEY_NUMBERS.VISIT_SUMMARY_PROD,
+      staging: SURVEY_NUMBERS.VISIT_SUMMARY_STAGING,
+    },
+  },
+};
 
 function getSurvey(buildtype, url) {
   const surveyData = medalliaSurveys;
