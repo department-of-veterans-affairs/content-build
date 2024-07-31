094929ed2e499af36fcf7409dee9cc04228556aa
Derek Houck <derek.houck@agile6.com>
2024-07-31 12:36:30 -0500

    Add normalizeForms step to postProcessDigitalForm

--- src/site/stages/build/drupal/static-data-files/digitalForm/postProcessDigitalForm.js
+++ src/site/stages/build/drupal/static-data-files/digitalForm/postProcessDigitalForm.js
@@ -1,3 +1,20 @@
-const postProcessDigitalForm = queryResult => queryResult;
+const extractForms = resultObject => resultObject.data.nodeQuery.entities;
+
+const normalizeForm = form => {
+  return {
+    id: form.nid,
+    title: form.entityLabel,
+  };
+};
+
+const normalizeForms = forms => forms.map(form => normalizeForm(form));
+
+const postProcessDigitalForm = queryResult => {
+  const resultObject = JSON.parse(queryResult);
+  const forms = extractForms(resultObject);
+  const normalizedForms = normalizeForms(forms);
+
+  return JSON.stringify(normalizedForms);
+};
 
 module.exports.postProcessDigitalForm = postProcessDigitalForm;
--- src/site/stages/build/drupal/static-data-files/digitalForm/postProcessDigitalForm.unit.spec.js
+++ src/site/stages/build/drupal/static-data-files/digitalForm/postProcessDigitalForm.unit.spec.js
@@ -5,8 +5,84 @@ import { expect } from 'chai';
 const { postProcessDigitalForm } = require('./postProcessDigitalForm');
 
 describe('postProcessDigitalForm', () => {
-  it('returns queryResult with no changes', () => {
-    const queryResult = { data: { form: {} } };
-    expect(postProcessDigitalForm(queryResult)).to.eq(queryResult);
+  const queryResult = `
+{
+  "data": {
+    "nodeQuery": {
+      "entities": [
+        {
+          "nid": 71002,
+          "entityLabel": "Form with One Step",
+          "fieldVaFormNumber": "11111",
+          "fieldOmbNumber": "1111-1111",
+          "fieldChapters": [
+            {
+              "entity": {
+                "entityId": "157904",
+                "type": {
+                  "entity": {
+                    "entityId": "digital_form_name_and_date_of_bi",
+                    "entityLabel": "Name and Date of Birth"
+                  }
+                },
+                "fieldTitle": "The Only Step",
+                "fieldIncludeDateOfBirth": true
+              }
+            }
+          ]
+        },
+        {
+          "nid": 71004,
+          "entityLabel": "Form with Two Steps",
+          "fieldVaFormNumber": "222222",
+          "fieldOmbNumber": "1212-1212",
+          "fieldChapters": [
+            {
+              "entity": {
+                "entityId": "157906",
+                "type": {
+                  "entity": {
+                    "entityId": "digital_form_name_and_date_of_bi",
+                    "entityLabel": "Name and Date of Birth"
+                  }
+                },
+                "fieldTitle": "First Step",
+                "fieldIncludeDateOfBirth": true
+              }
+            },
+            {
+              "entity": {
+                "entityId": "157907",
+                "type": {
+                  "entity": {
+                    "entityId": "digital_form_name_and_date_of_bi",
+                    "entityLabel": "Name and Date of Birth"
+                  }
+                },
+                "fieldTitle": "Second Step",
+                "fieldIncludeDateOfBirth": false
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+    `;
+
+  let parsedResult;
+
+  beforeEach(() => {
+    const processedResult = postProcessDigitalForm(queryResult);
+    parsedResult = JSON.parse(processedResult);
+  });
+
+  it('returns a normalized JSON object', () => {
+    const testForm = parsedResult[1];
+
+    expect(parsedResult.length).to.eq(2);
+    expect(testForm.id).to.eq(71004);
+    expect(testForm.title).to.eq('Form with Two Steps');
   });
 });
