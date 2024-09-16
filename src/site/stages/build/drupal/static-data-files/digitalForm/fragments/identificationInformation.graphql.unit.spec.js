b64c733b627e85f08ebf8967e7f9c7d4b9f932e0
Derek Houck <derek.houck@agile6.com>
2024-09-16 14:07:01 -0500

    Add Identification Information fragment

---
+++ src/site/stages/build/drupal/static-data-files/digitalForm/fragments/identificationInformation.graphql.unit.spec.js
@@ -0,0 +1,15 @@
+/* eslint-disable @department-of-veterans-affairs/axe-check-required */
+
+import { expect } from 'chai';
+import identificationInformation from './identificationInformation.graphql';
+
+describe('identificationInformation fragment', () => {
+  it('includes fieldTitle', () => {
+    expect(identificationInformation).to.have.string('fieldTitle');
+  });
+  it('includes fieldIncludeVeteranSService', () => {
+    expect(identificationInformation).to.have.string(
+      'fieldIncludeVeteranSService',
+    );
+  });
+});
