647111449b4f38f9968c6a4a444d45091870d658
Eli Selkin <5606931+eselkin@users.noreply.github.com>
2024-10-29 17:35:02 -0700

    VACMS 17867 - Telephone paragraph integration (#2317)
    * VACMS 17867 - MH phone number now fieldTelephone phone number paragraph
    * fix oddity when making MH phone a fax number
    * add one more explicit phone number type to a section - not SL
    * VACMS 17865 - Conditionally render fieldTelephone when present, otherwise fieldPhoneNumber (#2325)
    * staff phone
    * no message-aria-describedby for fax, tty, or sms
    * add feature flag checks
    * enabled Feature flags
    * VACMS 17866 billing and insurance phone number paragraphs (#2330)
    * phoneType not phoneNumberType
    * remove space in template string
    * 2 spaces

--- src/site/stages/build/drupal/graphql/paragraph-fragments/staffProfile.paragraph.graphql.js
+++ src/site/stages/build/drupal/graphql/paragraph-fragments/staffProfile.paragraph.graphql.js
@@ -1,3 +1,5 @@
+const personTelephoneParagraphGraphql = require('./personTelephone.paragraph.graphql');
+
 /**
  * A Drupal paragraph containing rich text.
  *
@@ -17,6 +19,7 @@ module.exports = `
         fieldDescription
         fieldEmailAddress
         fieldPhoneNumber
+        ${personTelephoneParagraphGraphql}
         fieldCompleteBiographyCreate
         fieldIntroText
         fieldBody {
