c9cc6aa30519c1cf740fdfd64fc8864f6df5c73a
Tim Cosgrove <tcosgrove@metrostar.com>
2024-11-26 16:06:43 -0800

    Fix botched for loop.

--- src/site/stages/build/plugins/modify-dom/check-broken-links/helpers/isBrokenLink.js
+++ src/site/stages/build/plugins/modify-dom/check-broken-links/helpers/isBrokenLink.js
@@ -28,7 +28,7 @@ function isBrokenLink(link, pagePath, allPaths) {
   let filePath = decodeURIComponent(parsed.pathname);
 
   // Check for link destinations we are not testing.
-  for (let i = 0; i < IGNORE_PATTERNS; i += 1) {
+  for (let i = 0; i < IGNORE_PATTERNS.length; i += 1) {
     if (filePath.match(IGNORE_PATTERNS[i])) {
       // eslint-disable-next-line no-console
       console.log(filePath);
