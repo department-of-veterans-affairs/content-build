ccc3d9af85f57dfa1c2cb19f0e7dbe53deda1975
Bryan Thompson <18094023+SnowboardTechie@users.noreply.github.com>
2025-01-08 06:36:15 -0800

    Don't build health care region pages for Manila

--- src/site/stages/build/drupal/process-manila-pages.js
+++ src/site/stages/build/drupal/process-manila-pages.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-param-reassign */
+const { ENTITY_BUNDLES } = require('../../../constants/content-modeling');
+
 const MANILA_VA_CLINIC_ENTITY_ID = '1187';
 
 function getManilaClinicUrl(path) {
@@ -10,6 +12,13 @@ function getManilaClinicUrl(path) {
 function isManilaVAClinicPage(page) {
   return (
     page?.fieldAdministration?.entity?.entityId === MANILA_VA_CLINIC_ENTITY_ID
+  );
+}
+
+function isManillaVaRegionHomepage(page) {
+  return (
+    isManilaVAClinicPage(page) &&
+    page?.entityBundle === ENTITY_BUNDLES.HEALTH_CARE_REGION_PAGE
   );
 }
 
@@ -53,6 +62,10 @@ function processManilaPages(drupalData) {
       if (isManilaVAClinicPage(page)) {
         acc.manilaVAClinicPages.push(page);
       } else {
+        // Federal Region Homepage should not be created for Manila VA Clinic
+        if (isManillaVaRegionHomepage(page)) {
+          return acc;
+        }
         acc.otherPages.push(page);
       }
 
