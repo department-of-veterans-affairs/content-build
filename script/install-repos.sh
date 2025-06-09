bf9ff7fb539cd142a24d2f5197710fcb6fe0da78
Tim Cosgrove <tcosgrove@metrostar.com>
2025-06-09 16:19:04 -0700

    Shift from shallow to treeless git clones of support repos.

--- script/install-repos.sh
+++ script/install-repos.sh
@@ -1,27 +1,18 @@
 #!/usr/bin/env bash
 if [ ! -d ../vagov-content ]; then
-  git clone --single-branch --depth 1 https://github.com/department-of-veterans-affairs/vagov-content.git ../vagov-content
+  git clone --filter=tree:0 https://github.com/department-of-veterans-affairs/vagov-content.git ../vagov-content
 else
   echo "Repo vagov-content already cloned."
 fi
 
 if [ ! -d ../vets-website ]; then
-  git clone --single-branch --depth 1 https://github.com/department-of-veterans-affairs/vets-website.git ../vets-website
+  git clone --filter=tree:0 https://github.com/department-of-veterans-affairs/vets-website.git ../vets-website
 else
   echo "Repo vets-website already cloned."
 fi
 
 if [ ! -d ../vets-api ]; then
-  git clone --single-branch --depth 1 https://github.com/department-of-veterans-affairs/vets-api.git ../vets-api
+  git clone --filter=tree:0 https://github.com/department-of-veterans-affairs/vets-api.git ../vets-api
 else
   echo "Repo vets-api already cloned."
-fi
-
-if [ "${RUN_NEXT_BUILD}" = "1" ]; then
-  if [ ! -d ../next-build ]; then
-    git clone --single-branch --depth 1 "https://va-cms-bot:${GITHUB_TOKEN}@github.com/department-of-veterans-affairs/next-build.git" ../next-build
-    cd ../next-build && yarn install
-  else
-    echo "Repo next-build already cloned."
-  fi
 fi
