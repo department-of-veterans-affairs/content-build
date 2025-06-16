ae2000cec0f8d69c642994c5e4d4e8c7ecc57704
Christian Burk <omahane@gmail.com>
2025-06-16 10:37:39 -0500

    VACMS-20493: Updates count, adds const back

--- src/site/stages/build/drupal/graphql/CountEntityTypes.graphql.js
+++ src/site/stages/build/drupal/graphql/CountEntityTypes.graphql.js
@@ -111,16 +111,6 @@ const CountEntityTypes = `
     count
   }
 
-  personProfile: nodeQuery(
-    filter: {
-      conditions: [
-        {field: "status", value: ["1"]},
-        {field: "type", value: ["person_profile"]}
-      ]}
-  	) {
-    count
-  }
-
   office: nodeQuery(
     filter: {
       conditions: [
