b46125a5dfc0177c63ca5ed877165d666bdbcb18
Christian Burk <766573+omahane@users.noreply.github.com>
2025-06-30 11:38:37 -0500

    VACMS-21017: Removes news release from queries (#2581)

--- src/site/constants/brokenLinkIgnorePatterns.js
+++ src/site/constants/brokenLinkIgnorePatterns.js
@@ -19,6 +19,7 @@
   */
 const IGNORE_PATTERNS = [
   /\/events($|\/)?/, // This ignores all links to Event and Event Listing pages.
+  /\/news-releases($|\/)?/, // This ignores all links to Press Release pages.
   /\/staff-profiles($|\/)?/, // This ignores all links to Staff Profile pages.
   /\/stories($|\/)?/, // This ignores all links to Stories and Story Listing pages.
 ];
