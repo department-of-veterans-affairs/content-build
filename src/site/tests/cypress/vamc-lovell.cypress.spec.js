4e0d3831308f67a10d04a7455420c593bfabe28a
Brian Seek <brian.seek@civicactions.com>
2025-09-12 08:36:45 -0700

    VACMS-20439: health care local facility post launch (#2649)
    * removed health_care_local_facility queries and added template comment.
    
    * removed from count query.
    
    * removed cypress test for related content

--- src/site/tests/cypress/vamc-lovell.cypress.spec.js
+++ src/site/tests/cypress/vamc-lovell.cypress.spec.js
@@ -19,35 +19,11 @@ describe('VAMC Lovell - All TRICARE pages with expected MHS Genesis Patient Port
       'https://my.mhsgenesis.health.mil/',
     );
   });
-
-  it('TRICARE Captain James A. Lovell Location has MHS Genesis Patient Portal link', () => {
-    cy.visit(
-      '/lovell-federal-health-care-tricare/locations/captain-james-a-lovell-federal-health-care-center/',
-    );
-    cy.injectAxeThenAxeCheck();
-
-    verifyActionLink(
-      'MHS Genesis Patient Portal',
-      'https://my.mhsgenesis.health.mil/',
-    );
-  });
 });
 
 describe('VAMC Lovell - All VA pages with expected Make an appointment Top Task have it', () => {
   it('VA Health services has Make an appointment link', () => {
     cy.visit('/lovell-federal-health-care-va/health-services/');
-    cy.injectAxeThenAxeCheck();
-
-    verifyActionLink(
-      'Make an appointment',
-      '/lovell-federal-health-care-va/make-an-appointment',
-    );
-  });
-
-  it('VA Captain James A. Lovell Location has Make an appointment link', () => {
-    cy.visit(
-      '/lovell-federal-health-care-va/locations/captain-james-a-lovell-federal-health-care-center/',
-    );
     cy.injectAxeThenAxeCheck();
 
     verifyActionLink(
