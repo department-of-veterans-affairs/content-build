42e5e801a6aac24527a93e08a0d067ad8f8fe21c
Brian Seek <brian.seek@civicactions.com>
2025-10-06 14:55:56 -0700

    remove queries and add comment to tempalte for vet center and vbs pages

--- src/site/stages/build/drupal/graphql/GetAllPages.graphql.js
+++ src/site/stages/build/drupal/graphql/GetAllPages.graphql.js
@@ -122,7 +122,6 @@ const buildQuery = () => {
         ... nodeBasicLandingPage
         ... nodeCampaignLandingPage
         ... policiesPageFragment
-        ... vbaFacilityFragment
       }
     }`;
 
