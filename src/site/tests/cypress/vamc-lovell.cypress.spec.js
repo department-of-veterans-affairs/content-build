e042c04a4e30187a56a2ab8313672e4e2ac66621
Patrick Wolfert <patrickrwolfert@gmail.com>
2025-12-10 15:55:31 -0800

    This isn't used anymore in this test

--- src/site/tests/cypress/vamc-lovell.cypress.spec.js
+++ src/site/tests/cypress/vamc-lovell.cypress.spec.js
@@ -1,14 +1,3 @@
-const verifyActionLink = (expectedText, expectedHref) =>
-  cy
-    .get('va-link-action')
-    .eq(0)
-    .shadow()
-    .find('a')
-    .should('be.visible')
-    .should('have.text', expectedText)
-    .should('have.attr', 'href')
-    .and('include', expectedHref);
-
 describe('VAMC Lovell - VA Make an appointment page does not have TRICARE experience switcher', () => {
   it('VA Make an appointment page does not have TRICARE experience switcher', () => {
     cy.visit('/lovell-federal-health-care-va/make-an-appointment/');
