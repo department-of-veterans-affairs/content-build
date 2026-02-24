7e89e91cad742b33e0e18e8a81e9877d025a13ee
Eli Selkin <5606931+eselkin@users.noreply.github.com>
2026-02-24 14:08:59 -0800

    configurable host

--- src/site/stages/build/drupal/static-data-files/vamcSystem/index.js
+++ src/site/stages/build/drupal/static-data-files/vamcSystem/index.js
@@ -2,7 +2,8 @@ const { postProcessVamcSystems } = require('./postProcessVamcSystem');
 
 // URLs to fetch (even if they are local files)
 const query = [
-  'http://prod.cms.va.gov/admin/content/exports/vamc-facilities-csv',
+  `${process.env.DRUPAL_ADDRESS ??
+    'https://prod.cms.va.gov'}/admin/content/exports/vamc-facilities-csv`,
 ];
 
 const postProcess = postProcessVamcSystems;
