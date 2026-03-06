2354a8476f90176e449b201a9579055fa602bf6a
Patrick Wolfert <patrickrwolfert@gmail.com>
2026-03-06 13:28:43 -0800

    The table logged by the build script was missing 10 content types (#2868)
    The counts here looked like this:
    
    ```
    ┌────────────────────────────┬───────┐
    │          (index)           │ count │
    ├────────────────────────────┼───────┤
    │        benefitPages        │  416  │
    │           vaForm           │  409  │
    │ healthCareRegionDetailPage │ 6192  │
    │           office           │   1   │
    │           nodeQa           │  326  │
    └────────────────────────────┴───────┘
    ```
    
    and if you add all those counts up, they total 7344. However, later the output says
    
    ```
    Finished 339 queries in 102s with 7510 pages
    ```
    
    There were 166 pages missing in this table. After this change, this is the new table, which does total 7510:
    
    ```
    ┌────────────────────────────┬───────┐
    │          (index)           │ count │
    ├────────────────────────────┼───────┤
    │        benefitPages        │  416  │
    │           vaForm           │  409  │
    │ healthCareRegionDetailPage │ 6192  │
    │           office           │   1   │
    │           nodeQa           │  326  │
    │        landingPage         │  13   │
    │     publicationListing     │   1   │
    │       faqMultipleQa        │  23   │
    │         stepByStep         │  12   │
    │      mediaListImages       │   0   │
    │         checklist          │   2   │
    │      mediaListVideos       │   1   │
    │ supportResourcesDetailPage │  101  │
    │      basicLandingPage      │   1   │
    │    campaignLandingPage     │  12   │
    └────────────────────────────┴───────┘
    ```

--- src/site/stages/build/drupal/graphql/CountEntityTypes.graphql.js
+++ src/site/stages/build/drupal/graphql/CountEntityTypes.graphql.js
@@ -50,6 +50,106 @@ const CountEntityTypes = `
   	) {
     count
   }
+
+  landingPage: nodeQuery(
+    filter: {
+      conditions: [
+        {field: "status", value: ["1"]},
+        {field: "type", value: ["landing_page"]}
+      ]}
+  	) {
+    count
+  }
+
+  publicationListing: nodeQuery(
+    filter: {
+      conditions: [
+        {field: "status", value: ["1"]},
+        {field: "type", value: ["publication_listing"]}
+      ]}
+  	) {
+    count
+  }
+
+  faqMultipleQa: nodeQuery(
+    filter: {
+      conditions: [
+        {field: "status", value: ["1"]},
+        {field: "type", value: ["faq_multiple_q_a"]}
+      ]}
+  	) {
+    count
+  }
+
+  stepByStep: nodeQuery(
+    filter: {
+      conditions: [
+        {field: "status", value: ["1"]},
+        {field: "type", value: ["step_by_step"]}
+      ]}
+  	) {
+    count
+  }
+
+  mediaListImages: nodeQuery(
+    filter: {
+      conditions: [
+        {field: "status", value: ["1"]},
+        {field: "type", value: ["media_list_images"]}
+      ]}
+  	) {
+    count
+  }
+
+  checklist: nodeQuery(
+    filter: {
+      conditions: [
+        {field: "status", value: ["1"]},
+        {field: "type", value: ["checklist"]}
+      ]}
+  	) {
+    count
+  }
+
+  mediaListVideos: nodeQuery(
+    filter: {
+      conditions: [
+        {field: "status", value: ["1"]},
+        {field: "type", value: ["media_list_videos"]}
+      ]}
+  	) {
+    count
+  }
+
+  supportResourcesDetailPage: nodeQuery(
+    filter: {
+      conditions: [
+        {field: "status", value: ["1"]},
+        {field: "type", value: ["support_resources_detail_page"]}
+      ]}
+  	) {
+    count
+  }
+
+  basicLandingPage: nodeQuery(
+    filter: {
+      conditions: [
+        {field: "status", value: ["1"]},
+        {field: "type", value: ["basic_landing_page"]}
+      ]}
+  	) {
+    count
+  }
+
+  campaignLandingPage: nodeQuery(
+    filter: {
+      conditions: [
+        {field: "status", value: ["1"]},
+        {field: "type", value: ["campaign_landing_page"]}
+      ]}
+  	) {
+    count
+  }
 }
 `;
 
