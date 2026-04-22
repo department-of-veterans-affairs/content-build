7139ae17f575356cfe83a48343f7265f092b0d65
Patrick Wolfert <patrickrwolfert@gmail.com>
2026-04-22 10:23:47 -0700

    I have a feeling it doesn't actually successfully load this page

--- src/site/tests/cypress/vamc-lovell.cypress.spec.js
+++ src/site/tests/cypress/vamc-lovell.cypress.spec.js
@@ -1,6 +1,9 @@
 describe('VAMC Lovell - VA Make an appointment page does not have TRICARE experience switcher', () => {
   it('VA Make an appointment page does not have TRICARE experience switcher', () => {
     cy.visit('/lovell-federal-health-care-va/make-an-appointment/');
+    cy.findByRole('heading', { name: 'Make an appointment', level: 1 }).should(
+      'be.visible',
+    );
     cy.injectAxeThenAxeCheck();
     cy.findByTestId('lovell-switch-link').should('not.exist');
   });
